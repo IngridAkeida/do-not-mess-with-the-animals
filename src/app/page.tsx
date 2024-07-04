cb9c9db4022a8c08d78a11999b190eb7784c1bdd
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-04 14:41:20 +0200

    chore: Add Component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header/Header';
 import Main from '../components/Main/Main';
 import Footer from '../components/Footer/Footer';
+import Component from '../components/Component';
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Main />
       <Footer />
+      <Component />
     </div>
   );
 }
---
+++ src/components/Component.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function MyComponent() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const encodedTerm = encodeURIComponent(searchTerm);
+      const response = await fetch(`/api/serverDataDDD?query=${encodedTerm}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setResult(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        className='text-black'
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search..."
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {error && <div>Error: {error}</div>}
+      
+      {result && (
+        <div>
+          <h2>Results:</h2>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
