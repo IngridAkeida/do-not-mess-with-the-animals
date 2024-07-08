12897c07481fa8d0db592c8de753aedfea960e32
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-08 17:45:04 +0200

    refactor: Update Header Banner component styles for better responsiveness

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-
 const Banner = () => {
   return (
-    <div className='h-96 bg-blue-700 flex flex-col items-center justify-center gap-4 px-10'>
-      <h1 className='text-5xl'>Welcome to our page</h1>
-      <p className='text-2xl'>Browse movies and TV shows to ensure they are free from animal violence.</p>
-      <div className='text-2xl relative'>
-        <input type='text' placeholder='Search' className='p-4 w-screen max-w-5xl bg-blue-100 rounded-xl'/>
-        <button className='p-4 bg-blue-800 rounded-xl absolute right-0 hover:text-black'>Search</button>
+    <div className='text-center px-2 h-96 bg-blue-700 flex flex-col items-center justify-center gap-4 sm:px-10'>
+      <h1 className='text-center text-xl md:text-5xl text-white'>Welcome to our page</h1>
+      <p className='text-base md:text-xl'>Browse movies and TV shows to ensure they are free from animal violence.</p>
+      <div className='text-base sm:text-2xl relative m-2'>
+        <input type='text' placeholder='Search' className='p-2 sm:p-4 sm:w-screen max-w-5xl bg-blue-100 rounded-xl'/>
+        <button className='p-2 sm:p-4 bg-blue-800 rounded-xl absolute right-0 hover:text-black'>Search</button>
       </div>
     </div>
   );
