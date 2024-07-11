742f7a67b5717ecb7d1f8145b4151dfed959c2db
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-11 18:09:34 +0200

    refactor: Update landing page banner color and search input styles

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -11,20 +11,20 @@ const Banner = () => {
   }
 
   return (
-    <div className='text-center px-2 h-96 bg-amber-700 flex flex-col items-center justify-center gap-4 sm:px-10 rounded-b-md'>
-      <h1 className='text-center text-xl md:text-5xl text-white'>Welcome to our page</h1>
+    <div className='text-center px-2 h-96 bg-gradient-to-br from-teal-600 to-blue-900 flex flex-col items-center justify-center gap-4 sm:px-10 rounded-b-md text-blue-200'>
+      <h1 className='text-center text-xl md:text-5xl'>Welcome to our page</h1>
       <p className='text-base md:text-xl'>Browse movies and TV shows to ensure they are free from animal violence and others triggers</p>
       <div className='text-base sm:text-2xl relative m-2'>
         <input 
-          className='p-2 sm:p-4 sm:w-screen max-w-5xl bg-amber-100 rounded-xl'
+          className='p-2 sm:p-4 sm:w-screen max-w-5xl bg-blue-100 rounded-xl text-blue-900'
           type='text' 
-          placeholder='Search...' 
+          placeholder='Search for movies or tv shows...' 
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           />
         <button 
-        className='p-2 sm:p-4 bg-amber-800 hover:bg-amber-900 rounded-xl absolute right-0 
-        text-white hover:text-amber-100'
+        className='p-2 sm:p-4 bg-teal-600 hover:bg-blue-900 rounded-xl absolute right-0 
+        text-white hover:text-blue-100'
         onClick={handleClick}>Search</button>
       </div>
     </div>
