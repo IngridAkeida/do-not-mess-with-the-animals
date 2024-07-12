1658881347004c9099e7f6af2ab27da001a91fc3
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-12 11:54:59 +0200

    refactor: Update movie page trigger details display and add caution message

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -4,6 +4,8 @@ import Nav from '../../../components/Header/Nav/Nav';
 import Main from '../../../components/Main/Main';
 import Footer from '../../../components/Footer/Footer';
 
+import TriggerResult from '../../../components/uiComponents/triggerResult/triggerResult';
+
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
@@ -48,25 +50,6 @@ const Movie = () => {
 
   const stylesPoster ='w-32 md:w-20 max-h-96 object-contain rounded-md shadow-md';
 
-  //yesSum totals 
-  const totalYesSum = triggers.reduce((total, trigger) => {
-    const triggerTotal = trigger.topics.reduce((triggerTotal, topic) => {
-      return triggerTotal + topic.yesSum;
-    }, 0);
-    return total + triggerTotal;
-  }, 0);
-
-  //noSum totals
-  const totalnoSum = triggers.reduce((total, trigger) => {
-    const triggerTotal = trigger.topics.reduce((triggerTotal, topic) => {
-      return triggerTotal + topic.noSum;
-    }, 0);
-    return total + triggerTotal;
-  }, 0);
-
-  //total votes 
-  const totalVotes = totalYesSum + totalnoSum;
-
   return (
     <div className='max-w-7xl mx-auto bg-blue-950 text-white'>
       <Nav/>
@@ -88,32 +71,7 @@ const Movie = () => {
           </div>
           <p className='font-semibold bg-gradient-to-t from-black to-transparent px-4 pb-4'>{item.overview}</p>
         </div>
-      <div className=''>
-        <div className='bg-blue-900 m-2 rounded-md p-4 mb-2'>
-          <h1 className='text-center px-2 mb-2'><span className='font-bold'>{item.name}</span> has <span className='font-bold'>{triggers.length}</span> possible triggers with <span className='font-bold'>{totalYesSum}</span> votes in favor out of a total of <span className='font-bold'>{totalVotes}</span> general votes.</h1>
-          <h4 className='bg-red-900 px-2 text-center rounded-md text-bold'>Please review the list of triggers carefully and exercise caution if you are sensitive to any of them.</h4>
-        </div>
-        {triggers.map((trigger, index) => (
-          <div key={index}>
-            <h2 className='font-bold pl-4 mb-2'>{trigger.name}</h2>
-            <ul>
-              {trigger.topics.map((topic, index) => (
-                <li key={index}>
-                  <div className='bg-blue-900 m-2 rounded-md p-4 mb-2'>
-                    <h2>{topic.doesName}?</h2>
-                    <div className='flex gap-2 text-center items-center pt-1'>
-                      <p className='bg-red-500 text-center rounded-full w-20 h-auto'>Yes: {topic.yesSum}</p>
-                      <p className='bg-green-500 text center rounded-full w-20 h-auto'>No: {topic.noSum}</p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-
-      </div>
-        
+      <TriggerResult triggers={triggers} item={item}/>
       </main>
     </div>
   );
---
+++ src/components/uiComponents/triggerResult/triggerResult.jsx
@@ -0,0 +1,54 @@
+const triggerResult = ({triggers, item}) => {
+  //yesSum totals 
+  const totalYesSum = triggers.reduce((total, trigger) => {
+    const triggerTotal = trigger.topics.reduce((triggerTotal, topic) => {
+      return triggerTotal + topic.yesSum;
+    }, 0);
+    return total + triggerTotal;
+  }, 0);
+
+  //noSum totals
+  const totalnoSum = triggers.reduce((total, trigger) => {
+    const triggerTotal = trigger.topics.reduce((triggerTotal, topic) => {
+      return triggerTotal + topic.noSum;
+    }, 0);
+    return total + triggerTotal;
+  }, 0);
+
+  //total votes 
+  const totalVotes = totalYesSum + totalnoSum;
+
+
+  return (
+
+    <div className=''>
+      <div className='bg-blue-900 m-2 rounded-md p-4 mb-2'>
+        <h1 className='text-center px-2 mb-2'><span className='font-bold'>{item.name}</span> has <span className='font-bold'>{triggers.length}</span> possible triggers with <span className='font-bold'>{totalYesSum}</span> votes in favor out of a total of <span className='font-bold'>{totalVotes}</span> general votes.</h1>
+        <h4 className='bg-red-900 px-2 text-center rounded-md text-bold'>Please review the list of triggers carefully and exercise caution if you are sensitive to any of them.</h4>
+      </div>
+      {triggers.map((trigger, index) => (
+        <div key={index}>
+          <h2 className='font-bold pl-4 mb-2'>{trigger.name}</h2>
+          <ul>
+            {trigger.topics.map((topic, index) => (
+              <li key={index}>
+                <div className='bg-blue-900 m-2 rounded-md p-4 mb-2'>
+                  <h2>{topic.doesName}?</h2>
+                  <div className='flex gap-2 text-center items-center pt-1'>
+                    <p className='bg-red-500 text-center rounded-full w-20 h-auto'>Yes: {topic.yesSum}</p>
+                    <p className='bg-green-500 text center rounded-full w-20 h-auto'>No: {topic.noSum}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
+    </div>  
+
+  );
+};
+
+export default triggerResult;
+  
