046cc7bd77c17721c4cac845c51bc847b3d331a3
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-12 13:05:58 +0200

    refactor: Update BannerResult component to use backdrop_path instead of backgroundImage
    This commit updates the BannerResult component to use the `backdrop_path` property instead of the `backgroundImage` property for setting the background image. This change aligns the component with the data structure provided by the server.
    
    By making this modification, we ensure that the correct image URL is used for the background image, enhancing the visual presentation of the component.
    
    Refactor the `BannerResult` component to update the `backgroundImage` variable to use `item.backdrop_path` instead of `item.backgroundImage`. Additionally, update the alt text for the `Image` component to use `item.title` instead of `item.name`.
    
    This change improves the consistency and accuracy of the BannerResult component.

--- src/components/Main/BannerResult/BannerResult.jsx
+++ src/components/Main/BannerResult/BannerResult.jsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 const BannerResult = ({item}) => {
-  const backgroundImage = item.backgroundImage
-  ? `https://www.doesthedogdie.com/content/1800/0/${item.backgroundImage}`
+  const backgroundImage = item.backdrop_path
+  ? `https://www.doesthedogdie.com/content/1800/0/${item.backdrop_path}`
   : '/assets/movie-nf.png';
 
   const stylesPoster ='w-32 md:w-20 max-h-96 object-contain rounded-md shadow-md';
@@ -10,13 +10,13 @@ const BannerResult = ({item}) => {
   return (
     <div className={`flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
           <div className='flex p-4'>
-          {item.posterImage === null ? (
+          {item.poster_path=== null ? (
               <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
             ) : (
-              <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/200/0/${item.posterImage}`} width={300} height={300} alt={item.name} />
+              <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
             )}
             <div className='pl-4'>
-              <h1 className='py-1 font-bold'>{item.name}</h1>
+              <h1 className='py-1 font-bold'>{item.title}</h1>
               <p>{item.releaseYear}</p>
               <p>genre</p>
               <p>direct by: sjjsjs</p>
