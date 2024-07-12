46c12021769f37b9fdf2504a15782c650385e1d9
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-12 12:59:11 +0200

    refactor: Combine movie details with data from TMDB API
    This commit combines the movie details fetched from the server with additional data obtained from the TMDB API. The TMDB data includes information such as the tmdbId and other relevant details. By combining these two sets of data, we can enhance the movie page with more comprehensive information.
    
    Refactor the `fetchDetails` function to include an additional fetch request to the TMDB API using the tmdbId extracted from the server data. Once the TMDB data is fetched, it is merged with the server data into a single object called `combineData`. This combined data is then set as the state using the `setCombineData` hook.
    
    This change improves the movie page by providing a more complete and enriched user experience.

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -11,35 +11,60 @@ import { useEffect, useState } from 'react';
 const Movie = () => {
 
   const { id } = useParams();
-  const [details, setDetails] = useState(null);
+  const [combineData, setCombineData] = useState(null);
 
   useEffect(() => {
     const fetchDetails = async () => {
       if (!id) return;
+
       try {
         const response = await fetch(`/api/serverDataDDDId?id=${id}`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         const data = await response.json();
         console.log('Data fetched:', data);
-        setDetails(data);
-      } catch (error) {
-        console.error('Fetch error:', error);
-      }
-    };
+    
+      //Extrat tmdbId from url
+      const tmdbId = data.item.tmdbId;
+      console.log('tmdbId:', tmdbId)
+
+      //Fetch data from tmdb
+      const tmdbData = await fetch(`/api/serverDataTMDB?id=${tmdbId}`).then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      });
+
+      console.log('Data fetched from second API:', tmdbData)
+
+      const combineData = {
+        ...data,
+        tmdbData
+      };
+
+      console.log('Combined data:', combineData)
+
+      setCombineData(combineData);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+
+  };
 
     if (id) {
       fetchDetails();
     }
   }, [id]);
 
-  if (!details) {
+  if (!combineData) {
     return <p>Carregando...</p>;
   }
 
-  const item = details.item;
-  const triggers = details.allGroups;
+  const item = combineData.item;
+  const triggers = combineData.allGroups;
 
   return (
     <div className='max-w-7xl mx-auto bg-blue-950 text-white'>
