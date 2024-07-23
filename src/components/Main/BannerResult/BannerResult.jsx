3f5f946aa2353af0605c4c5776ac6ab750880584
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-23 18:12:41 +0200

    refactor: Update BannerResult component to use backdrop_path instead of backgroundImage

--- src/components/Main/BannerResult/BannerResult.jsx
+++ src/components/Main/BannerResult/BannerResult.jsx
@@ -31,28 +31,57 @@ const BannerResult = ({item}) => {
   };
 
   return (
-    <div className={`flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-          <div className='flex p-4'>
-          {item.poster_path=== null ? (
-              <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
-            ) : (
-              <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
-            )}
-            <div className='pl-4'>
-              <h1 className='py-1 font-bold'>{item.title}</h1>
-              <p>{item.release_date}</p>
-              <p className="flex flex-wrap gap-1">{item.genres.map((genre, index) => (
-                  <span key={index} className={`px-1 mx-1 text-xs rounded-lg ${genreColors[genre.id]}`}>{genre.name}</span>
-              ))}</p>
-              <p>Created By: {item.created_by[0].name}</p>
-              <p>Written by: sjjsjs</p> 
-            </div>
-          </div>
-          <div className='px-4 bg-gradient-to-t from-black to-transparent'>
-            <p className='font-bold'>{item.tagline}</p>
-            <p className='font-semibold pb-4'>{item.overview}</p>
-          </div>  
+    <div>
+      <div className={`flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className='flex p-4'>
+      {item.poster_path=== null ? (
+          <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
+        ) : (
+          <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
+        )}
+        <div className='pl-4'>
+          <h1 className='py-1 font-bold'>{item.title}</h1>
+          <p>{item.release_date}</p>
+          <p className="flex flex-wrap gap-1">{item.genres.map((genre, index) => (
+              <span key={index} className={`px-1 mx-1 text-xs rounded-lg ${genreColors[genre.id]}`}>{genre.name}</span>
+          ))}</p>
+          <p>Created By: {item.created_by[0].name}</p>
+          <p>Written by: sjjsjs</p> 
         </div>
+      </div>
+      <div className='px-4 bg-gradient-to-t from-black to-transparent'>
+        <p className='font-bold'>{item.tagline}</p>
+        <p className='font-semibold pb-4'>{item.overview}</p>
+      </div>  
+      </div>
+      <div>
+        <p>{item.episode_run_time}</p>
+        <p>{item.status}</p>
+        <p>{item.vote_average}</p>
+        <p>{item.vote_count}</p>
+        <p>{item.popularity}</p>
+        <ul className='flex flex-wrap flex-row'>
+          {item.seasons.map((season, index) => (
+            <li key={index} className='flex flex-col w-80 p-4 rounded-md shadow-md bg-blue-500 my-2'>
+              <h2 className='text-center'>{season.name}</h2>
+              <div className='flex justify-center'>
+                {season.poster_path=== null ? (
+                  <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={season.name} />
+                ) : (
+                  <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/1200/0/${season.poster_path}`} width={300} height={300} alt={season.title} />
+                )}
+              </div>
+              <div>
+                <p>Number of Episodes: {season.episode_count}</p>
+                <p>Release Data: {season.air_date}</p>
+                <p>Votes: {season.vote_average}</p>
+                <p>{season.overview}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
