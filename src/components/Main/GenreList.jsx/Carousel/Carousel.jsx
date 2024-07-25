3b533b1875749513ff9a57cee4543e4fe0b8f7a8
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-25 19:30:35 +0200

    refactor: Update Carousel component to use lazy loading for images

--- src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
+++ src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Carousel = ({index, item}) => {
   return (
     <div key={index} className='border border-black w-52'>
-      <p>{item.title}</p>
+      <p>{item.title || item.name}</p>
       <Image src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} width={300} height={450} alt={item.title} />
     </div>
   );
