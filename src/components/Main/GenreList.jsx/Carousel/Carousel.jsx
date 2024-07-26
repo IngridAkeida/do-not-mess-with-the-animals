f66bccfeadf3026a0d3b4d5d7040236c9b23d0d1
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-26 16:28:11 +0200

    refactor: Update Carousel component to use rounded-xl class for image container
    This commit updates the Carousel component to use the `rounded-xl` class instead of `rounded-full` for the image container. This change improves the visual appearance of the Carousel by using a slightly different border radius.
    
    By using the `rounded-xl` class, we ensure that the image container has a rounded shape that is visually appealing and consistent with the overall design.
    
    This change enhances the layout of the Carousel component by updating the border radius of the image container.

--- src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
+++ src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Carousel = ({index, item}) => {
   return (
     <div key={index} className='h-60 w-40 relative'>
-      <div className='absolute text-white bg-gradient-to-t from-black to-transparent h-60 w-40 rounded-full flex flex-col justify-center items-center text-center'>
+      <div className='absolute text-white bg-gradient-to-t from-black to-transparent h-60 w-40 rounded-xl flex flex-col justify-center items-center text-center'>
         <div className='flex gap-2 justify-center items-center text-black'>
           <button className='w-6 bg-blue-50 rounded-md'>1</button>
           <button className='w-6 bg-blue-50 rounded-md'>2</button>
@@ -12,7 +12,7 @@ const Carousel = ({index, item}) => {
         <p className='p-2'>{item.title || item.name}</p>
         <div>Animal violence?</div>
       </div>
-      <Image src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-full'/>  
+      <Image src={`https://image.tmdb.org/t/p//w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-xl'/>  
     </div>
   );
 }
