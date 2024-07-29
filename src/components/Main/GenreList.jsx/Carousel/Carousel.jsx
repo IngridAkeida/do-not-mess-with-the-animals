de43cce33c2978507265486cf753b8f3d668f9b8
IngridAkeida <ingridsanchesp@gmail.com>
2024-07-29 23:37:36 +0200

    refactor: Update Carousel component to use smaller width for images in Carousel
    This commit updates the Carousel component to use a smaller width for images in the Carousel. By reducing the width, we improve the overall layout and ensure that the images fit within the designated space. This change enhances the visual presentation of the Carousel component, providing a more polished and cohesive design.

--- src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
+++ src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
@@ -2,15 +2,9 @@ import Image from "next/image";
 
 const Carousel = ({index, item}) => {
   return (
-    <div key={index} className='sm:h-60 sm:w-40 relative pr-2'>
-      <div className='absolute text-white bg-gradient-to-r  sm:bg-gradient-to-t from-black to-transparent h-[158px] sm:h-60 w-52 sm:w-40 rounded-xl flex flex-col justify-center items-center text-left'>
-        <p className='p-2 sm:hidden'>{item.title || item.name}</p>
-        <div>Animal violence?</div>
-        <div className='flex gap-2 justify-center items-center text-black'>
-          <button className='w-6 bg-blue-50 rounded-md'>1</button>
-          <button className='w-6 bg-blue-50 rounded-md'>2</button>
-          <button className='w-6 bg-blue-50 rounded-md'>3</button>
-        </div>
+    <div key={index} className='sm:h-60 sm:w-40 relative pr-2 sm:cursor-pointer'>
+      <div className='absolute text-white bg-gradient-to-r  sm:bg-gradient-to-t from-black to-transparent h-[158px] sm:h-60 w-52 sm:w-40 rounded-xl flex flex-col justify-center sm:justify-end items-center text-center sm:pb-4'>
+        <p className='p-2 '>{item.title || item.name}</p>
       </div>
       <div className='hidden sm:flex'>
         <Image src={`https://image.tmdb.org/t/p//w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-xl'/>  
