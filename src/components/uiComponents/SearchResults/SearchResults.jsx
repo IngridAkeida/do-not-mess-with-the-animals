74122863b1311876721f7235fc1f175b18d2d850
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-04 21:23:17 +0200

    refactor: Update Nav component styles for search input and button

--- src/components/uiComponents/SearchResults/SearchResults.jsx
+++ src/components/uiComponents/SearchResults/SearchResults.jsx
@@ -11,7 +11,8 @@ function reduceText(text, maxLength) {
 const SearchResults = ({results, searchTerm}) => {
   
   const maxLength = 100;
-  const stylesPoster ='w-64 md:w-20 h-auto max-h-96 object-contain rounded-md shadow-md';
+  const stylesPosterMobile ='w-64 h-auto max-h-96 object-contain rounded-md shadow-md';
+  const stylesPosterWeb ='sm:w-20 sm:max-h-60 object-contain rounded-md shadow-md';
 
   console.log('results', results);
 
@@ -20,11 +21,11 @@ const SearchResults = ({results, searchTerm}) => {
   }
 
   return (
-    <div className='bg-dark-primary-a40'>
-    <h2 className='pl-10 py-6 text-xl'>You searched for the term <span className='font-bold text-dark-menu-y10'>{searchTerm}</span>, we found <span className='font-bold text-dark-menu-y10'>{results.length}</span> contents with that keyword, of which <span className='font-bold text-dark-menu-y10'>x</span> are films and <span className='font-bold text-dark-menu-y10'>x</span> are tv shows</h2>
-    <div className='flex gap-4'>
-      <div className='w-1/5 border'>coluna 1</div>
-      <ul className='flex w-4/5 border flex-col md:flex-row md:flex-wrap gap-2 md:gap-4 justify-center items-center'>
+    <div className='bg-dark-neutral-a30'>
+    <h2 className='sm:pl-10 py-6 text-xl text-center'>You searched for the term <span className='font-bold text-dark-menu-y10'>{searchTerm}</span>, we found <span className='font-bold text-dark-menu-y10'>{results.length}</span> contents with that keyword, of which <span className='font-bold text-dark-menu-y10'>x</span> are films and <span className='font-bold text-dark-menu-y10'>x</span> are tv shows</h2>
+    <div className='flex gap-4 justify-center'>
+      <div className='hidden sm:block w-2/12'>coluna 1</div>
+      <ul className='flex w-10/12 flex-col md:flex-row md:flex-wrap gap-2 md:gap-4 justify-center items-center'>
       {results.map((result, index) => {
 
         let resultType = '';
@@ -38,53 +39,60 @@ const SearchResults = ({results, searchTerm}) => {
         console.log('resultType', resultType);
 
         return (
-        <li key={index} className='p-2 flex flex-col items-center justify-center md:flex-row gap-4 bg-dark-neutral-a30 rounded-md shadow-md w-72 md:h-96 relative'>
-          {result.posterImage === null ? (
-            <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={result.name} />
+        <li key={index} className='p-2 flex flex-col sm:flex-row items-center justify-center gap-4 bg-dark-primary-a40 rounded-md shadow-md w-60 sm:w-screen sm:h-40 relative sm:mr-4'>
+        <div className='block sm:hidden'>{result.backgroundImage=== null ? (
+            <Image className={stylesPosterMobile} src='/assets/movie-nf.png' width={300} height={300} alt={result.name} />
           ) : (
-            <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/200/0/${result.posterImage}`} width={300} height={300} alt={result.name} />
+            <Image className={stylesPosterMobile} src={`https://www.doesthedogdie.com/content/200/0/${result.backgroundImage}`} width={300} height={300} alt={result.name} />
           )}
-          <div className=''>
-            <h2 className='text-base text-center font-bold'>{result.name}</h2>
-            <p>Release data: {result.releaseYear}</p>
-            {result.genre && <p>Genre: {result.genre}</p>}
-            <div className='flex'>
-              {result.additionalData && result.additionalData.length > 0 && 
-                result.additionalData.map((data, index) => (
-                  data.name === 'Animal' && data.topics && data.topics.length > 0 && 
-                    data.topics.map((topic, topicIndex) => (
-                      topic.doesName === 'Does an animal die' && (
-                        <div className='bg-dark-neutral-a40 px-1 py-2 rounded-md' key={`${index}-${topicIndex}`}>
-                          <h2>{topic.doesName}?</h2>
+        </div>
+        <div className='hidden sm:block w-1/12'>{result.posterImage=== null ? (
+            <Image className={stylesPosterWeb} src='/assets/movie-nf.png' width={500} height={500} alt={result.name} />
+          ) : (
+            <Image className={stylesPosterWeb} src={`https://www.doesthedogdie.com/content/200/0/${result.posterImage}`} width={500} height={500} alt={result.name} />
+          )}
+        </div>
+        <div className='w-11/12'>
+          <h2 className='text-base font-bold'>{result.name}</h2>
+          <p>Release data: {result.releaseYear}</p>
+          {result.genre && <p>Genre: {result.genre}</p>}
+          <div className='flex'>
+            {result.additionalData && result.additionalData.length > 0 && 
+              result.additionalData.map((data, index) => (
+                data.name === 'Animal' && data.topics && data.topics.length > 0 && 
+                  data.topics.map((topic, topicIndex) => (
+                    topic.doesName === 'Does an animal die' && (
+                      <div className='bg-dark-neutral-a40 px-1 py-2 rounded-md' key={`${index}-${topicIndex}`}>
+                        <h2>{topic.doesName}?</h2>
 
-                          {topic.yesSum === 0 && topic.noSum === 0 ? (
-                            'no data'
-                          ) : (
-                            <div className='flex gap-2 mt-1 text-white'>
-                              <p className='bg-alert-danger-600 text-center rounded-full w-20 h-auto'> Yes {topic.yesSum}</p>
-                              <p className='bg-alert-success-600 text-center rounded-full w-20 h-auto'> No {topic.noSum}</p>
-                            </div>
-                          )}
-                        </div>
-                      )
-                    ))
-                ))
-              }
-            </div>
-            <p>Overview: {reduceText(result.overview, maxLength)}</p>
-            <div className=''>
-              <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 left-2 w-10'>
-                <Link href={`/${resultType}/${result.id}`}>➕</Link>
-              </button>
-              {/* <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 left-14 w-10'>
-                <Link href={`/${resultType}/${result.id}`}>❤️</Link>
-              </button>
-              <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 right-18 w-10'>
-                <Link href={`/${resultType}/${result.id}`}>-</Link>
-              </button> */}
-            </div>
-            
+                        {topic.yesSum === 0 && topic.noSum === 0 ? (
+                          'no data'
+                        ) : (
+                          <div className='flex gap-2 mt-1 text-white'>
+                            <p className='bg-alert-danger-600 text-center rounded-full w-20 h-auto'> Yes {topic.yesSum}</p>
+                            <p className='bg-alert-success-600 text-center rounded-full w-20 h-auto'> No {topic.noSum}</p>
+                          </div>
+                        )}
+                      </div>
+                    )
+                  ))
+              ))
+            }
           </div>
+          <p>Overview: {reduceText(result.overview, maxLength)}</p>
+          <div className=''>
+            <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 left-2 w-10'>
+              <Link href={`/${resultType}/${result.id}`}>+</Link>
+            </button>
+            {/* <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 left-14 w-10'>
+              <Link href={`/${resultType}/${result.id}`}>❤️</Link>
+            </button>
+            <button className='p-2 bg-teal-600 hover:bg-dark-neutral-a40 rounded-md text-white hover:text-blue-100 absolute bottom-2 right-18 w-10'>
+              <Link href={`/${resultType}/${result.id}`}>-</Link>
+            </button> */}
+          </div>
+          
+        </div>
         </li>
       )})}
     </ul>
