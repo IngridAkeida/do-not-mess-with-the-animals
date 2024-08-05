2fc1714973464d37ea18182d47766c654531f8b2
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-05 14:43:34 +0200

    refactor: Update Carousel component to handle null poster and backdrop paths

--- src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
+++ src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
@@ -1,40 +1,54 @@
 import Image from 'next/image';
-import { FaRegHeart } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa6";
-import { FaPlus } from "react-icons/fa6";
-import { FaCheck } from "react-icons/fa";
-import { FaPlay } from "react-icons/fa";
+import { useState } from 'react';
+import { FaRegHeart, FaHeart, FaPlus, FaCheck, FaPlay } from 'react-icons/fa';
 
+const Carousel = ({ index, item }) => {
+  const [isAdded, setIsAdded] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(false);
 
-const Carousel = ({index, item}) => {
+  const handleAddClick = () => {
+    setIsAdded(!isAdded);
+  };
+
+  const handleFavoriteClick = () => {
+    setIsFavorited(!isFavorited);
+  };
 
   return (
     <div key={index} className='sm:h-60 sm:w-40 relative pr-2 sm:cursor-pointer'>
-      <div className='absolute text-left text-white bg-gradient-to-r  sm:bg-gradient-to-t from-black hover:from-transparent to-transparent h-[158px] sm:h-[230px] w-52 sm:w-[152px] rounded-xl flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4 border'>
-      <div className='px-2'>
-        <p className='sm:hidden font-bold pb-1'>{item.title || item.name}</p>
-        <p className='text-sm'>{item.release_date}</p>
-        <p className='text-sm'>Trigger</p> 
-        <p className='text-sm'>yes X no</p>
-        <p className='text-sm'><FaCheck /><FaPlus/><FaRegHeart /><FaHeart /><FaPlay/></p>
-      </div>
+      <div className='absolute text-left text-white bg-gradient-to-r sm:bg-gradient-to-t from-black hover:from-transparent to-transparent h-[158px] sm:h-[230px] w-52 sm:w-[152px] rounded-xl flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4 border'>
+        <div className='px-2'>
+          <p className='sm:hidden font-bold pb-1'>{item.title || item.name}</p>
+          <p className='text-sm'>{item.release_date}</p>
+          <p className='text-sm'>Trigger</p> 
+          <p className='text-sm'>yes X no</p>
+          <div className='text-sm flex space-x-2'>
+            <span onClick={handleAddClick} className='cursor-pointer'>
+              {isAdded ? <FaCheck /> : <FaPlus />}
+            </span>
+            <span onClick={handleFavoriteClick} className='cursor-pointer'>
+              {isFavorited ? <FaHeart /> : <FaRegHeart />}
+            </span>
+            <FaPlay />
+          </div>
+        </div>
       </div>
       <div className='hidden sm:flex'>
-        {item.poster_path=== null ? (
+        {item.poster_path === null ? (
           <Image className='rounded-xl' src='/assets/movie-nf.png' width={300} height={300} alt={item.title} />
         ) : (
           <Image src={`https://image.tmdb.org/t/p//w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-xl'/>  
         )}
       </div>
       <div className='sm:hidden flex'>
-      {item.backdrop_path === null ? (
-        <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={item.title} />
+        {item.backdrop_path === null ? (
+          <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={item.title} />
         ) : (
-        <Image src={`https://image.tmdb.org/t/p//w400${item.backdrop_path}`} width={400} height={300} alt={item.title} className='rounded-xl'/> 
-      )} 
+          <Image src={`https://image.tmdb.org/t/p//w400${item.backdrop_path}`} width={400} height={300} alt={item.title} className='rounded-xl'/> 
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Carousel;
