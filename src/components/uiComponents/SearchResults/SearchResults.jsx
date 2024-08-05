317561663ce761f25111aa4c8c484251a38d5ea6
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-05 17:55:41 +0200

    refactor: Update SearchResults component to fix flexbox alignment and improve styling

--- src/components/uiComponents/SearchResults/SearchResults.jsx
+++ src/components/uiComponents/SearchResults/SearchResults.jsx
@@ -3,21 +3,11 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { FaRegHeart, FaHeart, FaPlus, FaCheck, FaPlay } from 'react-icons/fa';
 
-function reduceText(text, maxLength) {
-  if (!text || text.length <= maxLength) {
-    return text;
-  }
-  return text.slice(0, maxLength) + '...';
-};
-
 const SearchResults = ({results, searchTerm}) => {
 
   const [isAdded, setIsAdded] = useState(false);
   const [isFavorited, setIsFavorited] = useState(false);
-  
-  const maxLength = 100;
-  // const stylesPosterMobile ='w-64 h-auto max-h-96 object-contain rounded-md shadow-md';
-  // const stylesPosterWeb ='sm:w-20 sm:max-h-60 object-contain rounded-md shadow-md';
+  const [filter, setFilter] = useState('all');
 
   console.log('results', results);
 
@@ -25,7 +15,6 @@ const SearchResults = ({results, searchTerm}) => {
     return <h2>No results found for {searchTerm}</h2>;
   }
 
-  
 
   const handleAddClick = () => {
     setIsAdded(!isAdded);
@@ -35,14 +24,40 @@ const SearchResults = ({results, searchTerm}) => {
     setIsFavorited(!isFavorited);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredResults = results.filter(result => {
+    if (filter === 'all') {
+      return true;
+    }
+    if (filter === 'movies' && result.ItemTypeId !== 16) {
+      return true;
+    }
+    if (filter === 'tvshows' && result.ItemTypeId === 15) {
+      return true;
+    }
+    return false;
+  });
+
   return (
     <div className='bg-dark-neutral-a30 flex flex-col px-2 py-4 sm:px-0 gap-2'>
       <h2 className='bg-gradient-to-br from-dark-primary-a40 to-dark-primary-a30 font-semibold text-xl text-center bg-black rounded-md p-2 '>You searched for the term <span className='font-bold text-dark-menu-y10'>{searchTerm}</span>, we found <span className='font-bold text-dark-menu-y10'>{results.length}</span> contents with that keyword, of which <span className='font-bold text-dark-menu-y10'>x</span> are films and <span className='font-bold text-dark-menu-y10'>x</span> are tv shows</h2>
 
     <div className='flex justify-center'>
-      <div className='hidden sm:block w-2/12'>coluna 1</div>
+      <div className='hidden sm:block w-2/12'>
+        <div className='p-4  rounded-md shadow-md'>
+          <h3 className='text-lg font-semibold mb-2'>Filter by:</h3>
+          <select id="filter" value={filter} onChange={handleFilterChange} className='p-2 rounded-md w-full'>
+            <option value="all">All</option>
+            <option value="movies">Movies</option>
+            <option value="tvshows">TV Shows</option>
+          </select>
+        </div>
+      </div>
       <ul className=' flex flex-wrap justify-center p-2 gap-2 bg-gradient-to-br w-[100%] from-dark-primary-a40 to-dark-primary-a30 rounded-md sm:pb-8'>
-      {results.map((result, index) => {
+      {filteredResults.map((result, index) => {
 
         let resultType = '';
 
