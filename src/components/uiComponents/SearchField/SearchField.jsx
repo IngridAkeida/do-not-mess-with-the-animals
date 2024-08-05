d0fb4208fede969ab30c50c3bee17bdbc430a1bb
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-05 14:05:34 +0200

    refactor: Update SearchField component to handle empty search term
    This commit updates the SearchField component to handle cases where the search term is empty. If the search term is empty, an error message is displayed to the user indicating that the search field cannot be empty. This change improves the user experience by providing feedback and preventing invalid searches.

--- src/components/uiComponents/SearchField/SearchField.jsx
+++ src/components/uiComponents/SearchField/SearchField.jsx
@@ -4,11 +4,23 @@ import { useState } from 'react';
 const SearchField = ({wrapStyle, buttonStyle, inputStyle}) => {
 
   const [searchTerm, setSearchTerm] = useState(''); 
+  const [error, setError] = useState('');
   const router = useRouter();
 
   const handleClick = () => {
+    if (searchTerm.trim() === '') {
+      setError('The search field cannot be empty.');
+      return;
+    }
+    setError(''); 
     router.push(`/search?term=${encodeURIComponent(searchTerm)}`);
-  }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  };
 
   return (
       <div className={wrapStyle}>
@@ -18,10 +30,15 @@ const SearchField = ({wrapStyle, buttonStyle, inputStyle}) => {
           placeholder='Search for movies or tv shows...' 
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyPress}
           />
         <button 
         className={buttonStyle}
         onClick={handleClick}>Search</button>
+        <div className='h-6'>
+          {error && <p className='text-sm rounded-xl bg-alert-danger-600 text-alert-danger-100'>{error}</p>}
+        </div>
+        
       </div>
   );
 }
