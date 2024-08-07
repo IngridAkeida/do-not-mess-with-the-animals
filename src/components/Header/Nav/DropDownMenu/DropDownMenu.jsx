f3f83cb62880f8c05dc1ac3a903cdb7e3176490a
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-07 12:34:55 +0200

    refactor: Improve mobile menu functionality and styling

--- src/components/Header/Nav/DropDownMenu/DropDownMenu.jsx
+++ src/components/Header/Nav/DropDownMenu/DropDownMenu.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { NavigationInfo } from '../../../uiComponents/MenuList/MenuList';
 import { useRouter } from 'next/navigation';
 
@@ -6,6 +6,7 @@ const DropDownMenu = () => {
   const [openDropdown, setOpenDropdown] = useState(null);
   const navigationInfo = NavigationInfo;
   const router = useRouter();
+  const dropdownRef = useRef(null);
 
   const handleOptionClick = (href) => {
     router.push(href);
@@ -15,41 +16,54 @@ const DropDownMenu = () => {
     setOpenDropdown(openDropdown === name ? null : name);
   };
 
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setOpenDropdown(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className='hidden sm:flex lg:gap-x-6'>
-      {navigationInfo.map((item) => (
-        <div key={item.name} className='relative my-2 mx-1'>
-          <button
-            className='text-sm font-semibold leading-6 text-alert-danger-100 hover:text-dark-menu-y10'
-            onClick={() => toggleDropdown(item.name)}
-          >
-            {item.name}
-          </button>
-          {openDropdown === item.name && (
-            <div className='absolute top-full left-0 mt-1 bg-dark-neutral-a40 rounded shadow-lg'>
-              {item.subMenu
-                ? item.subMenu.map((subItem) => (
-                    <div
-                      key={subItem.name}
-                      className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10 hover:text-dark-neutral-a40'
-                      onClick={() => handleOptionClick(subItem.href)}
-                    >
-                      {subItem.name}
-                    </div>
-                  ))
-                : (
+    {navigationInfo.map((item) => (
+      <div key={item.name} className='relative mt-2 mx-1' ref={dropdownRef}>
+        <button
+          className='text-sm font-semibold leading-6 text-alert-danger-100 hover:text-dark-menu-y10'
+          onClick={() => toggleDropdown(item.name)}
+        >
+          {item.name}
+        </button>
+        {openDropdown === item.name && (
+          <div className='absolute top-11 left-0 bg-dark-neutral-a40 rounded shadow-lg border'>
+            {item.subMenu
+              ? item.subMenu.map((subItem) => (
                   <div
-                    className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10'
-                    onClick={() => handleOptionClick(item.href)}
+                    key={subItem.name}
+                    className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10 hover:text-dark-neutral-a40'
+                    onClick={() => handleOptionClick(subItem.href)}
                   >
-                    {item.name}
+                    {subItem.name}
                   </div>
-                )}
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+                ))
+              : (
+                <div
+                  className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10'
+                  onClick={() => handleOptionClick(item.href)}
+                >
+                  {item.name}
+                </div>
+              )}
+          </div>
+        )}
+      </div>
+    ))}
+  </div>
   );
 }
 
