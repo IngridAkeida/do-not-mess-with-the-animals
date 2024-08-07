8409a4634009fe3fef74a9d6a4dc9c5531d52af8
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-07 15:41:15 +0200

    refactor: Improve CardLayout component
    This commit refactors the CardLayout component to improve code readability and enhance the user experience. It updates the imports for the next/image and react-icons libraries, and adds state and event handlers for adding and favoriting items. Additionally, it improves the layout and styling of the component, including the text alignment, font weights, and spacing. These changes ensure consistency and improve the overall functionality of the CardLayout component.

--- src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
+++ src/components/Main/GenreList.jsx/Carousel/Carousel.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 import { FaRegHeart, FaHeart, FaPlus, FaCheck, FaPlay } from 'react-icons/fa';
 
@@ -14,41 +15,51 @@ const Carousel = ({ index, item }) => {
     setIsFavorited(!isFavorited);
   };
 
+  let resultType = '';
+
+  if (item.media_type === 'TV') {
+    resultType = 'tvshow';
+  } else {
+    resultType = 'movie';
+  }
+
   return (
-    <div key={index} className='sm:h-60 sm:w-40 relative pr-2 sm:cursor-pointer'>
-      <div className='absolute text-left text-white bg-gradient-to-r sm:bg-gradient-to-t from-black sm:hover:from-black sm:from-transparent to-transparent h-[100%] w-[100%] rounded-xl flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4'>
-        <div className='block sm:text-transparent sm:hover:text-white px-2 h-[80%] w-[100%]'>
-          <p className='font-bold'>{item.title || item.name}</p>
-          <p className='text-xs pb-1'>{item.release_date}</p>
-          <p className='text-xs'>genre,genre, genre</p>
-          <p className='text-sm'>Trigger</p> 
-          <p className='text-sm'>yes X no</p>
-          <div className='border text-sm flex  gap-2 sm:absolute sm:bottom-4 sm:left-12'>
-            <span onClick={handleAddClick} className='cursor-pointer'>
-              {isAdded ? <FaCheck /> : <FaPlus />}
-            </span>
-            <span onClick={handleFavoriteClick} className='cursor-pointer'>
-              {isFavorited ? <FaHeart /> : <FaRegHeart />}
-            </span>
-            <FaPlay />
+    <Link key={index} href={`/${resultType}/${item.id}`}>
+      <div key={index} className='sm:h-60 sm:w-40 relative pr-2 sm:cursor-pointer'>
+        <div className='absolute text-left text-white bg-gradient-to-r sm:bg-gradient-to-t from-black sm:hover:from-black sm:from-transparent to-transparent h-[100%] w-[100%] rounded-xl flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4'>
+          <div className='block sm:text-transparent sm:hover:text-white px-2 h-[80%] w-[100%]'>
+            <p className='font-bold'>{item.title || item.name}</p>
+            <p className='text-xs pb-1'>{item.release_date}</p>
+            <p className='text-xs'>genre,genre, genre</p>
+            <p className='text-sm'>Trigger</p> 
+            <p className='text-sm'>yes X no</p>
+            <div className='border text-sm flex  gap-2 sm:absolute sm:bottom-4 sm:left-12'>
+              <span onClick={handleAddClick} className='cursor-pointer'>
+                {isAdded ? <FaCheck /> : <FaPlus />}
+              </span>
+              <span onClick={handleFavoriteClick} className='cursor-pointer'>
+                {isFavorited ? <FaHeart /> : <FaRegHeart />}
+              </span>
+              <FaPlay />
+            </div>
           </div>
         </div>
+        <div className='hidden sm:flex'>
+          {item.poster_path === null ? (
+            <Image className='rounded-xl' src='/assets/movie-nf.png' width={300} height={300} alt={item.title} />
+          ) : (
+            <Image src={`https://image.tmdb.org/t/p//w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-xl'/>  
+          )}
+        </div>
+        <div className='sm:hidden flex'>
+          {item.backdrop_path === null ? (
+            <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={item.title} />
+          ) : (
+            <Image src={`https://image.tmdb.org/t/p//w400${item.backdrop_path}`} width={400} height={300} alt={item.title} className='rounded-xl'/> 
+          )}
+        </div>
       </div>
-      <div className='hidden sm:flex'>
-        {item.poster_path === null ? (
-          <Image className='rounded-xl' src='/assets/movie-nf.png' width={300} height={300} alt={item.title} />
-        ) : (
-          <Image src={`https://image.tmdb.org/t/p//w300${item.poster_path}`} width={300} height={450} alt={item.title} className='rounded-xl'/>  
-        )}
-      </div>
-      <div className='sm:hidden flex'>
-        {item.backdrop_path === null ? (
-          <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={item.title} />
-        ) : (
-          <Image src={`https://image.tmdb.org/t/p//w400${item.backdrop_path}`} width={400} height={300} alt={item.title} className='rounded-xl'/> 
-        )}
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/uiComponents/SearchResults/SearchResults.jsx
+++ src/components/uiComponents/SearchResults/SearchResults.jsx
@@ -1,12 +1,9 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-import { FaRegHeart, FaHeart, FaPlus, FaCheck, FaPlay } from 'react-icons/fa';
+import CardLayout from '../../Main/CardLayout/CardLayout';
 
 const SearchResults = ({results, searchTerm}) => {
 
-  const [isAdded, setIsAdded] = useState(false);
-  const [isFavorited, setIsFavorited] = useState(false);
   const [filter, setFilter] = useState('all');
 
   console.log('results', results);
@@ -14,15 +11,6 @@ const SearchResults = ({results, searchTerm}) => {
   if (!results) {
     return <h2>No results found for {searchTerm}</h2>;
   }
-
-
-  const handleAddClick = () => {
-    setIsAdded(!isAdded);
-  };
-
-  const handleFavoriteClick = () => {
-    setIsFavorited(!isFavorited);
-  };
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
@@ -71,62 +59,7 @@ const SearchResults = ({results, searchTerm}) => {
 
         return (
         <Link key={index} href={`/${resultType}/${result.id}`}>
-          <li className='sm:h-60 sm:w-40 relative sm:cursor-pointer'>
-            <div className='absolute text-left text-white bg-gradient-to-r sm:bg-gradient-to-t from-black sm:hover:from-black sm:from-transparent to-transparent h-[100%] w-[100%] rounded-md flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4'>
-              <div className='block sm:text-transparent sm:hover:text-white px-2 h-[80%] w-[100%]'>
-                <div className='flex sm:flex-col gap-2 sm:gap-0'>
-                  <p className='font-bold'>{result.name}</p>
-                  <p className='font-thin text-sm'>({result.itemType.name})</p>
-                </div>
-                <p className='text-xs pb-1'>{result.releaseYear}</p>
-                {result.genre && <p>Genre: {result.genre}</p>}
-                <div className=''>
-                  {result.additionalData && result.additionalData.length > 0 && 
-                    result.additionalData.map((data, index) => (
-                      data.name === 'Animal' && data.topics && data.topics.length > 0 && 
-                        data.topics.map((topic, topicIndex) => (
-                          topic.doesName === 'Does an animal die' && (
-                            <div className='' key={`${index}-${topicIndex}`}>
-                              <h2 className='font-light text-sm pb-1'>{topic.doesName}?</h2>
-
-                              {topic.yesSum === 0 && topic.noSum === 0 ? (
-                                'no data'
-                              ) : (
-                                <div className='flex sm:justify-center text-center gap-1 mb-1'>
-                                  <p className='bg-alert-danger-600 w-7 text-xs p-1 rounded-full'> {topic.yesSum}</p>
-                                  <p className='bg-alert-success-600 w-7 text-xs p-1 rounded-full'>{topic.noSum}</p>
-                                </div>
-                              )}
-                            </div>
-                          )
-                        ))
-                    ))
-                  }
-                </div>
-                <div className='text-sm flex gap-2 absolute bottom-2 sm:bottom-4 sm:left-12'>
-                  <span onClick={handleAddClick} className='cursor-pointer'>
-                    {isAdded ? <FaCheck /> : <FaPlus />}
-                  </span>
-                  <span onClick={handleFavoriteClick} className='cursor-pointer'>
-                    {isFavorited ? <FaHeart /> : <FaRegHeart />}
-                  </span>
-                  <FaPlay />
-                </div>
-              </div>
-            </div>
-            <div className='flex sm:hidden'>{result.backgroundImage === null ? (
-                  <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={result.name} />
-                ) : (
-                  <Image className='rounded-xl' src={`https://www.doesthedogdie.com/content/200/0/${result.backgroundImage}`} width={300} height={300} alt={result.name} />
-                )}
-              </div>
-            <div className='hidden sm:flex h-[100%]'>{result.posterImage=== null ? (
-                <Image className='rounded-xl' src='/assets/movie-nf.png' width={500} height={500} alt={result.name} />
-              ) : (
-                <Image className='rounded-xl' src={`https://www.doesthedogdie.com/content/200/0/${result.posterImage}`} width={500} height={500} alt={result.name} />
-              )}
-            </div>
-          </li>
+          <CardLayout result={result} />
         </Link>
       )})}
     </ul>
