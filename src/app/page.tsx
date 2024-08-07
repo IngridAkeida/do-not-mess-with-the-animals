147bb19ad5de502b5e56d794baf1b374f5ce49b9
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-07 16:14:38 +0200

    refactor: Update page components to improve code organization and user experience
    This commit updates the page components to improve code organization and enhance the user experience. It removes unused imports and commented-out code, ensuring a cleaner codebase. Additionally, it adjusts the layout and styling of the components to improve readability and consistency. These changes enhance the overall functionality and user experience of the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,9 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ComponentList from "../components/Main/GenreList.jsx/GenreList";
 
-import Login from "./login/page";
-import Signup from "./Signup/page";
-
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto bg-dark-neutral-a50">
-      {/* <Login />  */}
-      {/* < Signup /> */}
       <Header />
       <ComponentList />
       <Footer />
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,13 +2,15 @@
 import PrivateRoute from '../../components/PrivateRoute';
 import { useAuth } from '../../context/AuthContext';
 import { auth } from '../../pages/firebaseData';
+import Nav from '../../components/Header/Nav/Nav'
 
 const User = () => {
   const { user } = useAuth();
 
   return (
     <PrivateRoute>
-      <div className='text-black'>
+      <div className='max-w-7xl mx-auto bg-dark-neutral-a50'>
+        <Nav />
         <h1>Welcome, {user?.displayName || user?.email}!</h1>
         <div className='flex-col m-4'>
           <div className='w-24 h-24 bg-dark-neutral-a40 rounded-full text-white'>Image here</div>
