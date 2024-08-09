5c45fbc77c1e242bea4c10836f143a345cd2cd9a
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-09 17:17:33 +0200

    refactor: Update page component to use Nav and Banner components
    This commit updates the page component to use the Nav and Banner components instead of the Header component. The Nav component provides navigation functionality, while the Banner component adds a banner to the page. This change improves the structure and organization of the code, making it easier to maintain and enhance in the future.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client';
 
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import ComponentList from "../components/Main/GenreList.jsx/GenreList";
+import Nav from '../components/Header/Nav/Nav';
+import Banner from '../components/Header/Banner/Banner';
+import Footer from '../components/Footer/Footer';
+import ComponentList from '../components/Main/GenreList.jsx/GenreList';
 
 export default function Home() {
   return (
     <div className='max-w-7xl mx-auto h-96'>
-      <Header />
+      <Nav />
+      <Banner />
       <ComponentList />
       <Footer />
     </div>
