e5d5b7ee0aa6de662750a3cd62d5b26d83eec8f2
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-09 12:38:28 +0200

    refactor: Update People page component to include Nav component
    This commit adds the Nav component to the People page component in order to provide navigation functionality. The Nav component is imported and rendered within the People component, ensuring that the navigation menu is displayed on the page. This change improves the user experience by allowing users to easily navigate between different sections of the application.

---
+++ src/app/people/page.jsx
@@ -0,0 +1,13 @@
+import Nav from '../../components/Header/Nav/Nav';
+
+
+const People = () => {
+  return (
+    <div className='max-w-7xl mx-auto'>
+      <Nav />
+      <h1>People</h1>
+    </div>
+  );
+}
+
+export default People;
