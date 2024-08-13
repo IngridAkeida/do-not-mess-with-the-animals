0ebfc962eb9186afa2a0230bee84d63b3cc7f4ca
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-13 16:14:03 +0200

    refactor: Update import path for SearchResults component in search page

--- src/components/Main/SearchResults/SearchResults.jsx
+++ src/components/Main/SearchResults/SearchResults.jsx
@@ -37,10 +37,10 @@ const SearchResults = ({results, searchTerm}) => {
 
   return (
     <div className='bg-dark-neutral-a30 flex flex-col px-2 py-4 sm:px-0 gap-2'>
-      <h2 className='bg-gradient-to-br from-dark-primary-a40 to-dark-primary-a30 font-semibold text-xl text-center bg-black rounded-md p-2 '>You searched for the term <span className='font-bold text-dark-menu-y10'>{searchTerm}</span>, we found <span className='font-bold text-dark-menu-y10'>{filteredResults.length}</span> contents with that keyword, of which <span className='font-bold text-dark-menu-y10'>{movieCount}</span> are films and <span className='font-bold text-dark-menu-y10'>{tvShowCount}</span> are tv shows</h2>
+      <h2 className='bg-gradient-to-br from-dark-primary-a20 to-dark-primary-a40 font-semibold text-xl text-center bg-black rounded-md p-2 mx-2 '>You searched for the term <span className='font-bold text-dark-menu-y10'>{searchTerm}</span>, we found <span className='font-bold text-dark-menu-y10'>{filteredResults.length}</span> contents with that keyword, of which <span className='font-bold text-dark-menu-y10'>{movieCount}</span> are films and <span className='font-bold text-dark-menu-y10'>{tvShowCount}</span> are tv shows</h2>
 
     <div className='flex justify-center'>
-      <div className='hidden sm:block w-2/12'>
+      <div className='hidden sm:block w-3/12'>
         <div className='p-4  rounded-md shadow-md'>
           <h3 className='text-lg font-semibold mb-2'>Filter by:</h3>
           <select id='filter' value={filter} onChange={handleFilterChange} className='p-2 rounded-md w-full text-black'>
@@ -50,7 +50,7 @@ const SearchResults = ({results, searchTerm}) => {
           </select>
         </div>
       </div>
-      <ul className=' flex flex-wrap justify-center p-2 gap-2 bg-gradient-to-br w-[100%] from-dark-primary-a40 to-dark-primary-a30 rounded-md sm:pb-8'>
+      <ul className='flex flex-wrap justify-center p-2 gap-4 bg-gradient-to-br w-[100%] sm:w-9/12 from-dark-primary-a20 to-dark-primary-a40 rounded-md sm:pb-8 mx-2'>
       {filteredResults.map((result, index) => {
 
         let resultType = '';
