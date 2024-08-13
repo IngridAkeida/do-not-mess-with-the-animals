204d4803193dc6f392a81683023d31897174852f
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-13 17:53:57 +0200

    refactor: Update Settings component for autoplay feature
    Enable autoplay functionality in the Settings component by adding the 'autoplay' and 'autoplaySpeed' properties to the settings object. This change enhances the user experience by automatically advancing the slides every 3 seconds, providing a more dynamic and engaging carousel display.

--- src/components/uiComponents/Settings/Settings.jsx
+++ src/components/uiComponents/Settings/Settings.jsx
@@ -9,6 +9,8 @@ const settings = {
     slidesToScroll: 6,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1280,
