fa1fe72e09e137c96e5068081de926a0e9073b15
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-14 14:03:29 +0200

    refactor: Improve TV show display in BannerResult component

--- src/components/Main/BannerResult/BannerResult.jsx
+++ src/components/Main/BannerResult/BannerResult.jsx
@@ -24,30 +24,30 @@ const BannerResult = ({item, triggers}) => {
   return (
     <div>
       <div className={`flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className='flex p-4'>
-      {item.poster_path=== null ? (
-          <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
-        ) : (
-          <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
-        )}
-        <div className='pl-4'>
-          <h1 className='py-1 font-bold'>{item.name || item.title}</h1>
-          <p><FaPlay /></p>
-          {item.seasons && item.seasons.length > 0 ? (<p>TV show</p>) : (<p>Movie</p>)}
-          <p>{item.release_date}</p>
-          <p className="flex flex-wrap gap-1">{item.genres.map((genre, index) => (
-              <span key={index} className={`px-1 mx-1 text-xs rounded-lg ${genreColors[genre.id]}`}>{genre.name}</span>
-          ))}</p>
-          {item.seasons && item.seasons.length > 0 ? (<p>Seasons: {item.number_of_seasons}
-</p>) : null}
-          <p>Status: {item.status}</p>
-          {item.seasons && item.seasons.length > 0 ? (<p>Created By: {item.created_by[0].name}</p>) : null}
+        <div className='flex p-4'>
+        {item.poster_path=== null ? (
+            <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
+          ) : (
+            <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
+          )}
+          <div className='pl-4'>
+            <h1 className='py-1 font-bold'>{item.name || item.title}</h1>
+            <p><FaPlay /></p>
+            {item.seasons && item.seasons.length > 0 ? (<p>TV show</p>) : (<p>Movie</p>)}
+            <p>{item.release_date}</p>
+            <p className="flex flex-wrap gap-1">{item.genres.map((genre, index) => (
+                <span key={index} className={`px-1 mx-1 text-xs rounded-lg ${genreColors[genre.id]}`}>{genre.name}</span>
+            ))}</p>
+            {item.seasons && item.seasons.length > 0 ? (<p>Seasons: {item.number_of_seasons}
+  </p>) : null}
+            <p>Status: {item.status}</p>
+            {item.seasons && item.seasons.length > 0 ? (<p>Created By: {item.created_by[0].name}</p>) : null}
+          </div>
         </div>
-      </div>
-      <div className='px-4 bg-gradient-to-t from-black to-transparent'>
-        <p className='font-bold'>{item.tagline}</p>
-        <p className='font-semibold pb-4'>{item.overview}</p>
-      </div>  
+        <div className='px-4 bg-gradient-to-t from-black to-transparent'>
+          <p className='font-bold'>{item.tagline}</p>
+          <p className='font-semibold pb-4'>{item.overview}</p>
+        </div>  
       </div>
       <div>
         <CustomSlider
@@ -56,6 +56,7 @@ const BannerResult = ({item, triggers}) => {
           settings={settings}
           isSeason={true}
         />
+        <TriggerResult triggers={triggers} item={item}/>
         <CustomSlider
           title="Similar Titles"
           items={item.similar.results}
@@ -63,7 +64,7 @@ const BannerResult = ({item, triggers}) => {
           isSeason={false}
         />
       </div>
-      <TriggerResult triggers={triggers} item={item}/>
+      
     </div>
   );
 }
