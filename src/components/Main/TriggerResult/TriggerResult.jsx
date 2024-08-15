c076cf4f2c826742d71727bbd30fe16df2162efd
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-15 09:48:18 +0200

    refactor: Update TriggerResult component to handle empty triggers
    Handle the case when there are no triggers for a specific item in the TriggerResult component. Display a message indicating that there are no topics for the trigger. This improves the user experience by providing clear feedback when there is no data to display.

--- src/components/Main/TriggerResult/TriggerResult.jsx
+++ src/components/Main/TriggerResult/TriggerResult.jsx
@@ -18,6 +18,8 @@ const TriggerResult = ({triggers, item}) => {
   //total votes 
   const totalVotes = totalYesSum + totalnoSum;
 
+  console.log(triggers)
+
 
   return (
 
@@ -26,25 +28,30 @@ const TriggerResult = ({triggers, item}) => {
         <h1 className='text-center px-2 mb-2'><span className='font-bold'>{item.name}</span> has <span className='font-bold'>{triggers.length}</span> possible triggers with <span className='font-bold'>{totalYesSum}</span> votes in favor out of a total of <span className='font-bold'>{totalVotes}</span> general votes.</h1>
         <h4 className='bg-red-900 px-2 text-center rounded-md text-bold'>Please review the list of triggers carefully and exercise caution if you are sensitive to any of them.</h4>
       </div>
-      {triggers.map((trigger, index) => (
-        <div key={index}>
-          <h2 className='font-bold pl-4 mb-2'>{trigger.name}</h2>
-          <ul>
-            {trigger.topics.map((topic, index) => (
-              <li key={index}>
-                <div className='bg-dark-neutral-a40 m-2 rounded-md p-4 mb-2'>
-                  <h2>{topic.doesName}?</h2>
-                  <div className='flex gap-2 text-center items-center pt-1'>
-                    <p className='bg-red-500 text-center rounded-full w-20 h-auto'>Yes: {topic.yesSum}</p>
-                    <p className='bg-green-500 text center rounded-full w-20 h-auto'>No: {topic.noSum}</p>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-
+      {
+        triggers.length === 0 ? (
+            <div key={index}>
+              <p className='bg-dark-neutral-a40 m-2 rounded-md p-4 mb-2'>There are no topics for this trigger.</p>
+            </div>
+          ) : (
+            triggers.filter(trigger => trigger.name !== 'Unanswered Triggers').map((trigger, index) => (
+              <div key={index}>
+                <h2 className='font-bold pl-4 mb-2'>{trigger.name}</h2>
+                <ul>
+                  {trigger.topics.map((topic, index) => (
+                    <li key={index}>
+                      <div className='bg-dark-neutral-a40 m-2 rounded-md p-4 mb-2'>
+                        <h2>{topic.doesName}?</h2>
+                        <div className='flex gap-2 text-center items-center pt-1'>
+                          <p className='bg-red-500 text-center rounded-full w-20 h-auto'>Yes: {topic.yesSum}</p>
+                          <p className='bg-green-500 text center rounded-full w-20 h-auto'>No: {topic.noSum}</p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+          )))}
     </div>  
 
   );
