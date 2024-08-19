a7115111525d95ebb219809813d4f7d6e7c05921
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-19 12:22:48 +0200

    refactor: Update VideoModal component to improve code readability and remove unused dependencies

--- src/components/Main/BannerResult/BannerResult.jsx
+++ src/components/Main/BannerResult/BannerResult.jsx
@@ -1,79 +1,100 @@
+import { Fragment, useState } from 'react';
 import Image from "next/image";
 import GenreColors from "../../uiComponents/GenreColors/GenreColors";
-
 import { FaPlay } from "react-icons/fa";
-
+import VideoModal from '../../uiComponents/Modal/VideoModal';
 import CustomSlider from '../../uiComponents/CustomSlider/CustomSlider';
 import TriggerResult from '../../Main/TriggerResult/TriggerResult';
-
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Settings  from '../../uiComponents/Settings/Settings';
+import Settings from '../../uiComponents/Settings/Settings';
 
-const BannerResult = ({item, triggers}) => {
+const BannerResult = ({ item, triggers }) => {
+
+  const [showModal, setShowModal] = useState(false);
   const backgroundImage = item.backdrop_path
-  ? `https://www.doesthedogdie.com/content/1800/0/${item.backdrop_path}`
-  : '/assets/movie-nf.png';
+    ? `https://www.doesthedogdie.com/content/1800/0/${item.backdrop_path}`
+    : '/assets/movie-nf.png';
 
-  const stylesPoster ='w-32 md:w-24 max-h-96 object-cover rounded-md shadow-md';
+  const stylesPoster = 'w-32 md:w-24 max-h-96 object-cover rounded-md shadow-md';
 
   const genreColors = GenreColors;
   const settings = Settings;
-  console.log(item)
 
   return (
-    <div>
-      <div className={`flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className='flex'>
-          <div className='flex p-4'>
-          {item.poster_path=== null ? (
-              <Image className={stylesPoster} src='/assets/movie-nf.png' width={300} height={300} alt={item.name} />
-            ) : (
-              <Image className={stylesPoster} src={`https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`} width={300} height={300} alt={item.title} />
-            )}
-            <div className='pl-4'>
-              <div className='flex items-center'>
-                <h1 className='py-1 font-bold'>{item.name || item.title}</h1>
-                <p className='font-semibold text-sm ml-2'>({item.seasons && item.seasons.length > 0 ? ('TV Show') : ('Movie')})</p>
-                <p className='ml-2'><FaPlay /></p>
-              </div>
-              <p className="flex flex-wrap gap-1">{item.genres.map((genre, index) => (
-                  <span key={index} className={`px-1 text-xs rounded-lg ${genreColors[genre.id]}`}>{genre.name}</span>
-              ))}
-              </p>
-              <div className='text-sm'>
-                <p>{item.release_date || item.first_air_date}</p>
-                {item.seasons && item.seasons.length > 0 ? (<p><span className='font-semibold'>Seasons: </span>{item.number_of_seasons}</p>) : null}
-                <p><span className='font-semibold'>Status: </span>{item.status}</p>
-                {item.seasons && item.seasons.length > 0 ? (<p><span className='font-semibold'>Created By: </span>{item.created_by[0].name}</p>) : null}
+    <Fragment>
+      <div>
+        <div
+          className="flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          <div className="flex">
+            <div className="flex p-4">
+              <Image
+                className={stylesPoster}
+                src={item.poster_path
+                  ? `https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`
+                  : '/assets/movie-nf.png'}
+                width={300}
+                height={300}
+                alt={item.title || item.name}
+              />
+              <div className="pl-4">
+                <div className="flex items-center">
+                  <h1 className="py-1 font-bold">{item.name || item.title}</h1>
+                  <p className="font-semibold text-sm ml-2">
+                    {item.seasons && item.seasons.length > 0 ? 'TV Show' : 'Movie'}
+                  </p>
+                  <p className="ml-2" onClick={() => setShowModal(true)}><FaPlay /></p>
+                </div>
+                <p className="flex flex-wrap gap-1">
+                  {item.genres.map((genre) => (
+                    <span
+                      key={genre.id}
+                      className={`px-1 text-xs rounded-lg ${genreColors[genre.id]}`}
+                    >
+                      {genre.name}
+                    </span>
+                  ))}
+                </p>
+                <div className="text-sm">
+                  <p>{item.release_date || item.first_air_date}</p>
+                  {item.seasons && item.seasons.length > 0 && (
+                    <p><span className="font-semibold">Seasons: </span>{item.number_of_seasons}</p>
+                  )}
+                  <p><span className="font-semibold">Status: </span>{item.status}</p>
+                  {item.seasons && item.seasons.length > 0 && (
+                    <p><span className="font-semibold">Created By: </span>{item.created_by[0]?.name}</p>
+                  )}
+                </div>
               </div>
             </div>
+            <div>sklsklks</div>
           </div>
-          <div>sklsklks</div>
+
+          <div className="px-4 bg-gradient-to-t from-black to-transparent">
+            <p className="font-bold">{item.tagline}</p>
+            <p className="font-semibold pb-4">{item.overview}</p>
+          </div>
         </div>
-        
-        <div className='px-4 bg-gradient-to-t from-black to-transparent'>
-          <p className='font-bold'>{item.tagline}</p>
-          <p className='font-semibold pb-4'>{item.overview}</p>
-        </div>  
+        <div>
+          <CustomSlider
+            title="Seasons"
+            items={item.seasons}
+            settings={settings}
+            isSeason={true}
+          />
+          <TriggerResult triggers={triggers} item={item} />
+          <CustomSlider
+            title="Similar Titles"
+            items={item.similar?.results}
+            settings={settings}
+            isSeason={false}
+          />
+        </div>
       </div>
-      <div>
-        <CustomSlider
-          title="Seasons"
-          items={item.seasons}
-          settings={settings}
-          isSeason={true}
-        />
-        <TriggerResult triggers={triggers} item={item}/>
-        <CustomSlider
-          title="Similar Titles"
-          items={item.similar.results}
-          settings={settings}
-          isSeason={false}
-        />
-      </div>
-      
-    </div>
+      <VideoModal isVisible={showModal} onClose={() => setShowModal(false)} />
+    </Fragment>
   );
 }
 
--- src/components/uiComponents/Modal/VideoModal.jsx
+++ src/components/uiComponents/Modal/VideoModal.jsx
@@ -1,65 +1,17 @@
-import { useState, useEffect } from 'react';
-import Modal from 'react-modal';
+// import { useState, useEffect } from 'react';
 
-const VideoModal = ({ isOpen, onRequestClose, item }) => {
-  const [videos, setVideos] = useState([]);
+const VideoModal = ({isVisible, onClose}) => {
 
-  useEffect(() => {
-    if (item?.id) {
-      const fetchVideos = async () => {
-        try {
-          const response = await fetch(`/api/serverDataTMDB?id=${item.id}`);
-          if (!response.ok) {
-            throw new Error('Failed to fetch videos');
-          }
-          const data = await response.json();
-          setVideos(data.results);
-        } catch (error) {
-          console.error('Error fetching videos:', error);
-        }
-      };
+  if (!isVisible) return null;
   
-      fetchVideos(item.id); 
-    }
-  }, [item]);
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Video Modal"
-      overlayClassName="overlay"
-      className="modal-content" // Optional: Add specific class for modal content
-      shouldCloseOnOverlayClick={true} // Optional: Close on overlay click
-    >
-      <button 
-        onClick={onRequestClose} 
-        aria-label="Close modal"
-        className="close-button" // Optional: Add specific class for close button
-      >
-        X
-      </button>
-      <h2>{item.title || item.name}</h2>
-      {videos.length > 0 ? (
-        <div>
-          {videos.map(video => (
-            <div key={video.id}>
-              <h3>{video.name}</h3>
-              <iframe
-                width="560"
-                height="315"
-                src={`https://www.youtube.com/embed/${video.key}`}
-                title={video.name}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>No videos available</p>
-      )}
-    </Modal>
+    <div className="fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <div className="w-[600px] flex flex-col">
+        <button className="place-self-end" onClick={() => onClose()}>x</button>
+        <div className="bg-white text-black">Modal</div>
+      </div>
+    </div>
   );
 };
 
