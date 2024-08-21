ea34767e609578bdc766b6c528c33c8be3fa2993
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-21 17:32:22 +0200

    refactor: Improve TriggerMessage component wording and add video playback to BannerResult
    Enhance the wording in the TriggerMessage component to provide a clearer description of the trigger list, improving user understanding and caution. Additionally, embed a YouTube video iframe in the BannerResult component to enable video playback, enhancing the user experience by allowing them to watch related videos within the application.

--- src/components/Main/BannerResult/BannerResult.jsx
+++ src/components/Main/BannerResult/BannerResult.jsx
@@ -30,95 +30,104 @@ const BannerResult = ({ item, triggers }) => {
         <div
           className='flex-col flex gap-2 justify-start w-auto h-auto bg-cover bg-center'
           style={{ backgroundImage: `url(${backgroundImage})` }}
-        >
-          <div className='flex'>
-            <div className='flex p-4 relative'>
-              <Image
-                className={stylesPoster}
-                src={item.poster_path
-                  ? `https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`
-                  : '/assets/movie-nf.png'}
-                width={300}
-                height={300}
-                alt={item.title || item.name}
-              />
-              <div className='pl-4 border'>
+        > 
+          <div className='bg-black bg-opacity-35'>
+            <div className='flex flex-col'>
+              <div className='flex gap-2 items-center bg-dark-neutral-a30 bg-opacity-85 py-2'>
+                <h1 className='py-1 ml-4 font-bold text-5xl'>{item.name || item.title}</h1>
+                <p className='font-light ml-2 text-4xl'>
+                  {item.seasons && item.seasons.length > 0 ? '(TV Show)' : '(Movie)'}
+                </p>
                 <div className='flex items-center'>
-                  <h1 className='py-1 font-bold'>{item.name || item.title}</h1>
-                  <p className='font-semibold text-sm ml-2'>
-                    {item.seasons && item.seasons.length > 0 ? 'TV Show' : 'Movie'}
-                  </p>
                   <div className={`transition-opacity duration-300 hover:cursor-pointer
                     ${showModal ? 'opacity-0' : 'opacity-100'}`} 
                     onClick={() => setShowModal(true)}>
-                      <FaPlay className="pl-2 text-xl text-dark-primary-a10 transition-all hover:size-6  duration-300 hover:text-dark-primary-a20 "/>
-                    </div>
-                </div>
-                <p className='flex flex-wrap gap-1'>
-                  {item.genres.map((genre) => (
-                    <span
-                      key={genre.id}
-                      className={`px-1 text-xs rounded-lg ${genreColors[genre.id]}`}
-                    >
-                      {genre.name}
-                    </span>
-                  ))}
-                </p>
-                <div className='text-sm'>
-                  <p>{item.release_date || item.first_air_date}</p>
-                  {item.seasons && item.seasons.length > 0 && (
-                    <p><span className='font-semibold'>Seasons: </span>{item.number_of_seasons}</p>
-                  )}
-                  <p><span className='font-semibold'>Status: </span>{item.status}</p>
-                  {item.seasons && item.seasons.length > 0 && (
-                    <p><span className='font-semibold'>Created By: </span>{item.created_by[0]?.name}</p>
-                  )}
+                      <FaPlay className='pl-2 text-4xl text-dark-primary-a10 transition-all hover:size-10 duration-300 hover:text-dark-primary-a20'/>
+                  </div>
                 </div>
               </div>
-              <div>
-                <TriggerMessage item={item} triggers={triggers}/>
+              <div className='flex p-4 relative'>
+                <Image
+                  className={stylesPoster}
+                  src={item.poster_path
+                    ? `https://www.doesthedogdie.com/content/1200/0/${item.poster_path}`
+                    : '/assets/movie-nf.png'}
+                  width={300}
+                  height={300}
+                  alt={item.title || item.name}
+                />
+                <div className='flex flex-col pl-4 gap-2'>
+                  <div className='flex'>
+                    <div className=''>
+                      <p className='flex flex-wrap gap-1'>
+                        {item.genres.map((genre) => (
+                          <span
+                            key={genre.id}
+                            className={`px-1 text-xs rounded-lg ${genreColors[genre.id]}`}
+                          >
+                            {genre.name}
+                          </span>
+                        ))}
+                      </p>
+                      <div className='text-sm'>
+                        <p>{item.release_date || item.first_air_date}</p>
+                        {item.seasons && item.seasons.length > 0 && (
+                          <p><span className='font-semibold'>Seasons: </span>{item.number_of_seasons}</p>
+                        )}
+                        <p><span className='font-semibold'>Status: </span>{item.status}</p>
+                        {item.seasons && item.seasons.length > 0 && (
+                          <p><span className='font-semibold'>Created By: </span>{item.created_by[0]?.name}</p>
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <TriggerMessage item={item} triggers={triggers}/>
+                    </div>
+                  </div>
+                  <div>
+                    <h2>Cast:</h2>
+                    <div className='flex gap-2'>
+                      {item.credits.cast.map((cast, index) => (
+                      <div key={index} className=''>
+                        <div className=''>
+                          <Image
+                            className='rounded-md h-44 w-auto'
+                            src={cast.profile_path
+                              ? `https://www.doesthedogdie.com/content/1200/0/${cast.profile_path}`
+                              : '/assets/person-nf.png'}
+                            width={200}
+                            height={200}
+                            alt={cast.name}
+                          />
+                        </div>
+                        <div className=''>
+                          <h2 className='font-bold'>{cast.name}</h2>
+                          <p className='text-sm'>{cast.character}</p>
+                        </div>
+                      </div>
+                      ))}
+                    </div>  
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className='px-4 bg-gradient-to-t from-black to-transparent'>
-            <p className='font-bold'>{item.tagline}</p>
-            <p className='font-semibold pb-4'>{item.overview}</p>
-            <CustomSlider
-            title='Seasons'
-            items={item.seasons}
-            settings={settings}
-            isSeason={true}
-          />
+            <div className='px-4 bg-gradient-to-t from-black to-transparent'>
+              <p className='font-bold'>{item.tagline}</p>
+              <p className='font-semibold pb-4'>{item.overview}</p>
+              <CustomSlider
+              title='Seasons'
+              items={item.seasons}
+              settings={settings}
+              isSeason={true}
+            />
+            </div>
           </div>
         </div>
         <div>
         <div className='flex gap-2'>
           <TriggerResult triggers={triggers} item={item} />
-          <div className='w-[620px] flex flex-row flex-wrap gap-
-          2 justify-center items-center bg-dark-neutral-a40 rounded-md shadow-md p-2 m-2 gap-2'>
-            {item.credits.cast.map((cast, index) => (
-            <div key={index} className='bg-dark-neutral-a30 p-6 flex flex-col justify-center items-center w-48'>
-              <div className='w-32 h-auto'>
-                <Image
-                  className='rounded-md'
-                  src={cast.profile_path
-                    ? `https://www.doesthedogdie.com/content/1200/0/${cast.profile_path}`
-                    : '/assets/person-nf.png'}
-                  width={200}
-                  height={200}
-                  alt={cast.name}
-                />
-              </div>
-              <div className='pt-2 flex flex-col justify-center items-center'>
-                <h2 className='font-bold'>{cast.name}</h2>
-                <p className='text-sm'>{cast.character}</p>
-              </div>
-            </div>
-            ))}
-          </div>
-        </div>
           
+        </div>
           <CustomSlider
             title='Similar Titles'
             items={item.similar?.results}
