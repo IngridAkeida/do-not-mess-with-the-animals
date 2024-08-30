54ed4455c6da310196c68606cd2ca2cec0f62745
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-30 09:26:45 +0200

    refactor: Add lazy loading for user profile image and improve image handling
    This commit adds lazy loading functionality for the user profile image in the `User` component. It improves the image handling by allowing users to upload and remove their profile images. The `handleUpload` function handles the upload of the selected image file to Firebase storage, while the `handleRemoveImage` function removes the current profile image. The `handleUpdateProfile` function updates the user's profile with the new image URL. These changes enhance the performance and user experience of the user profile feature.

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,36 +1,107 @@
-'use client';
-import PrivateRoute from '../../components/PrivateRoute';
+'use client'
+import { useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
-import { auth } from '../../pages/firebaseData';
-import Nav from '../../components/Header/Nav/Nav'
+import { storage, firestore, auth } from '../../pages/firebaseData'; 
+import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
+import { doc, updateDoc } from "firebase/firestore";
+import Image from 'next/image';
 
 const User = () => {
-  const { user } = useAuth();
+  const { user, updateUserProfile } = useAuth();
+  const [profileImage, setProfileImage] = useState(user?.photoURL || '');
+  const [uploading, setUploading] = useState(false);
+
+  const handleUpdateProfile = async () => {
+    try {
+      if (user) {
+        await updateUserProfile({
+          displayName: 'Novo Nome',
+          photoURL: profileImage // Use the updated profileImage state
+        });
+        console.log('Perfil atualizado com sucesso!');
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar o perfil: ', error);
+    }
+  };
+
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!user) return; 
+
+    const file = e.target.files?.[0] || null;
+    if (!file) return;
+
+    const storageRef = ref(storage, `profileImages/${user.uid}`);
+    setUploading(true);
+
+    try {
+      await uploadBytes(storageRef, file);
+      const downloadURL = await getDownloadURL(storageRef);
+      await updateUserProfile({ photoURL: downloadURL }); 
+      await updateDoc(doc(firestore, 'users', user.uid), { photoURL: downloadURL });
+
+      setProfileImage(downloadURL);
+    } catch (error) {
+      console.error("Erro ao fazer upload da imagem: ", error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleRemoveImage = async () => {
+    if (!user) return;
+
+    const storageRef = ref(storage, `profileImages/${user.uid}`);
+    setUploading(true);
+
+    try {
+      await deleteObject(storageRef);
+      await updateUserProfile({ photoURL: null }); 
+      await updateDoc(doc(firestore, 'users', user.uid), { photoURL: null });
+      setProfileImage('');
+    } catch (error) {
+      console.error("Erro ao remover a imagem: ", error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  console.log('user', user);
+  console.log('profileImage', profileImage);
 
   return (
-    <PrivateRoute>
-      <div className='max-w-7xl mx-auto bg-dark-primary-a20'>
-        <Nav />
-        <h1>Welcome, {user?.displayName || user?.email}!</h1>
-        <div className='flex m-2'>
-          <div className='w-1/6 border'>
-            <div className='w-24 h-24 bg-dark-neutral-a40 rounded-full text-white'></div>
-            <h1>{user?.displayName || user?.email}</h1>
-            <h2>Member since 2021</h2>
-            <span>Edit profile</span>
-            <div>Settings</div>
-            <button onClick={() => auth.signOut()}>Sign Out</button>
+    <div className='max-w-7xl mx-auto bg-dark-primary-a20'>
+      <h1>Welcome, {user?.displayName || user?.email}!</h1>
+      {/* <p> User since {user?.auth.metadata.creationTime} </p> */}
+      {/* <p> User since {user?.auth.metadata.lastSignInTime} </p> */}
+      <div className='flex m-2'>
+        <div className='w-1/6 border'>
+          <div className='w-24 h-24 border rounded-full text-white'>
+            {profileImage ? (
+              <Image src={profileImage} alt="Profile" className='w-full h-full rounded-full' width={300} height={300} layout='fixed' />
+            ) : (
+              <span>No Image</span>
+            )}
           </div>
-          <div className='w-5/6 border'>
-            Your Lists
-            <div>Favorites</div>
-            <div>Watchlist</div>
-            <div>Watchedlist</div>
-            <div>Blocklist</div>
-          </div>
+          <h2>Member since 2021</h2>
+          <input type="file" onChange={handleUpload} disabled={uploading} />
+          <button onClick={handleRemoveImage} disabled={uploading || !profileImage}>
+            Remove Image
+          </button>
+          <span>Edit profile</span>
+          <button onClick={handleUpdateProfile}>Atualizar Perfil</button>
+          <div>Settings</div>
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+        <div className='w-5/6 border'>
+          Your Lists
+          <div>Favorites</div>
+          <div>Watchlist</div>
+          <div>Watchedlist</div>
+          <div>Blocklist</div>
         </div>
       </div>
-    </PrivateRoute>
+    </div>
   );
 };
 
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import { auth } from '../pages/firebaseData'; // Ajuste o caminho conforme necessário
+import { onAuthStateChanged, User, updateProfile as firebaseUpdateProfile } from 'firebase/auth';
+import { auth } from '../pages/firebaseData'; 
 
 interface AuthContextType {
-  user: User | null; // Altere para 'user' para ser consistente com o provider
+  user: User | null;
+  updateUserProfile: (profileData: { displayName?: string; photoURL?: string | null }) => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -21,8 +22,25 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     return () => unsubscribe();
   }, []);
 
+  const updateUserProfile = async (profileData: { displayName?: string; photoURL?: string }) => {
+    if (user) {
+      try {
+        await firebaseUpdateProfile(user, profileData);
+        setUser({
+          ...user,
+          displayName: profileData.displayName || user.displayName,
+          photoURL: profileData.photoURL || user.photoURL
+        });
+      } catch (error) {
+        console.error('Erro ao atualizar o perfil: ', error);
+      }
+    } else {
+      throw new Error('No user is currently signed in.');
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, updateUserProfile }}>
       {children}
     </AuthContext.Provider>
   );
