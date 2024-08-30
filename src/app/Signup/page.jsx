275ff4fb042295febc6de6d6adb3ad9c8af369d7
IngridAkeida <ingridsanchesp@gmail.com>
2024-08-30 17:57:52 +0200

    refactor: Improve user profile image handling and add lazy loading

--- src/app/Signup/page.jsx
+++ src/app/Signup/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { auth } from '../../pages/firebaseData';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import Nav from '../../components/Header/Nav/Nav';
 import Link from 'next/link';
 
 const SignUp = () => {
@@ -41,64 +42,67 @@ const SignUp = () => {
 };
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-custom text-dark-neutral-a40'>
-      <div className='bg-gradient-to-br from-dark-primary-a10 to-dark-menu-y10 p-8 rounded shadow-md w-80'>
-        <h2 className='text-2xl mb-4 text-center'>Sign Up</h2>
-        <form onSubmit={handleSignup}>
-          <div className='mb-4'>
-            <label className='block'>Name</label>
-            <input
-              type='text'
-              value={name}
-              onChange={handleNameChange}
-              className='w-full px-3 py-2 border rounded'
-              required
-            />
+    <div className='max-w-7xl mx-auto bg-dark-primary-a40'>
+      <Nav />
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-dark-primary-a30 via-dark-primary-a20 to-dark-primary-a30 text-dark-neutral-a40'>
+        <div className='bg-dark-accent-a40 p-8 rounded shadow-md w-80 border border-dark-menu-y10'>
+          <h2 className='text-2xl mb-4 text-center font-extrabold text-dark-menu-y10'>Sign Up</h2>
+          <form onSubmit={handleSignup}>
+            <div className='mb-4'>
+              <label className='block font-bold text-dark-menu-y10'>Name</label>
+              <input
+                type='text'
+                value={name}
+                onChange={handleNameChange}
+                className='w-full px-3 py-2 border rounded'
+                required
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='block font-bold text-dark-menu-y10'>Email</label>
+              <input
+                type='email'
+                value={email}
+                onChange={handleEmailChange}
+                className='w-full px-3 py-2 border rounded'
+                required
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='block font-bold text-dark-menu-y10'>Password</label>
+              <input
+                type='password'
+                value={password}
+                onChange={handlePasswordChange}
+                className='w-full px-3 py-2 border rounded'
+                required
+              />
+            </div>
+            <div className='mb-4'>
+              <label className='block font-bold text-dark-menu-y10'>Re-enter password</label>
+              <input
+                type='password'
+                value={confPassword}
+                onChange={handleConfirmPasswordChange}
+                className='w-full px-3 py-2 border rounded'
+                required
+              />
+            </div>
+            <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
+              Sign Up
+            </button>
+          </form>
+          <div className='mt-4 text-center'>
           </div>
-          <div className='mb-4'>
-            <label className='block'>Email</label>
-            <input
-              type='email'
-              value={email}
-              onChange={handleEmailChange}
-              className='w-full px-3 py-2 border rounded'
-              required
-            />
-          </div>
-          <div className='mb-4'>
-            <label className='block'>Password</label>
-            <input
-              type='password'
-              value={password}
-              onChange={handlePasswordChange}
-              className='w-full px-3 py-2 border rounded'
-              required
-            />
-          </div>
-          <div className='mb-4'>
-            <label className='block'>Re-enter password</label>
-            <input
-              type='password'
-              value={confPassword}
-              onChange={handleConfirmPasswordChange}
-              className='w-full px-3 py-2 border rounded'
-              required
-            />
-          </div>
-          <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
-            Sign Up
-          </button>
-        </form>
-        <div className='mt-4 text-center'>
-        </div>
-        <div className='flex text-sm gap-2 justify-center'>
-          <p className=''> Already have an account?</p>
-          <Link href='/login'>
-            <button type='button' className='hover:text-dark-accent-a20'>Sign in</button>
-          </Link>
+          <div className='flex text-sm gap-2 justify-center mt-1'>
+            <p className='text-white'> Already have an account?</p>
+            <Link href='/login'>
+              <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Sign in</button>
+            </Link>
+          </div> 
         </div> 
-      </div> 
-    </div>
+      </div>
+  </div>
   );
 };
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -30,47 +30,49 @@ const Login = () => {
   };
 
   return (
-    <div className=''>
+    <div className='max-w-7xl mx-auto bg-dark-primary-a40'>
       <Nav />
-      <div className='bg-gradient-to-br from-dark-primary-a10 to-dark-menu-y10 p-8 rounded shadow-md w-80 flex flex-col items-center justify-center min-h-screen bg-custom text-dark-neutral-a40'>
-        <h2 className='text-2xl mb-4 text-center'>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className='mb-4'>
-            <label className='block'>Email</label>
-            <input
-              type='email'
-              value={email}
-              onChange={handleEmailChange}
-              className='w-full px-3 py-2 border rounded'
-              placeholder='e-mail'
-              required
-            />
-          </div>
-          <div className='mb-4'>
-            <div className='flex justify-between items-end'>
-              <label className='block'>Password</label>
-              <button type='button' className='text-alert-info-600 text-xs hover:text-dark-accent-a20'>Forgot your password?</button>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-dark-primary-a30 via-dark-primary-a20 to-dark-primary-a30 text-dark-neutral-a40'>
+        <div className='bg-dark-accent-a40 p-8 rounded shadow-md w-80 border border-dark-menu-y10'>
+          <h2 className='text-2xl mb-4 text-center font-extrabold text-dark-menu-y10'>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className='mb-4'>
+              <label className='block font-bold text-dark-menu-y10'>Email</label>
+              <input
+                type='email'
+                value={email}
+                onChange={handleEmailChange}
+                className='w-full px-3 py-2 border rounded'
+                placeholder='e-mail'
+                required
+              />
             </div>
-            <input
-              type='password'
-              value={password}
-              onChange={handlePasswordChange}
-              className='w-full px-3 py-2 border rounded text-black'
-              placeholder='password'
-              required
-            />
+            <div className='mb-4'>
+              <div className='flex justify-between items-end'>
+                <label className='block font-bold text-dark-menu-y10'>Password</label>
+                <button type='button' className='text-white font-semibold pb-0.5 text-xs hover:text-dark-menu-y10'>Forgot your password?</button>
+              </div>
+              <input
+                type='password'
+                value={password}
+                onChange={handlePasswordChange}
+                className='w-full px-3 py-2 border rounded text-black'
+                placeholder='password'
+                required
+              />
+            </div>
+            <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
+              Login
+            </button>
+          </form>
+          <div className='flex text-sm gap-2 justify-center mt-1'>
+          <p className='text-white'> New here?</p>
+          <Link href='/signup'>
+            <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Create your account now!</button>
+          </Link>
           </div>
-          <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
-            Login
-          </button>
-        </form>
-        <div className='flex text-sm gap-2 justify-center'>
-        <p className=''> New here?</p>
-        <Link href='/signup'>
-          <button type='button' className='hover:text-dark-accent-a20'>Create your account now!</button>
-        </Link>
+          
         </div>
-        
       </div>
     </div>
   );
