67a764a07150c7139f911b258cadf7dedbb1e376
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-06 16:11:30 +0200

    refactor: Improve login page error handling and validation
    This commit refactors the login page to improve error handling and validation. It adds a function to validate the email format and displays appropriate error messages for incorrect email and password. Additionally, it updates the success message when the user logs in successfully. These changes enhance the user experience and ensure better error handling on the login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,23 +9,44 @@ import Link from 'next/link';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null); // Estado para mensagens de erro
   const router = useRouter();
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
 
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!isValidEmail(email)) {
+      setError('Email format is incorrect.');
+      return;
+    }
+
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log('user login succefully:', user);
+        console.log('User login successfully:', user);
         router.push('/user');
+        setError(null); 
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.error('login error:', errorCode, errorMessage);
+        if (errorCode.includes('wrong-password')) {
+          setError('The password is incorrect.');
+        } else if (errorCode.includes('user-not-found')) {
+          setError('No user found with this email.');
+        } else {
+          setError('An error occurred. Please try again.');
+        }
+
+        console.error('Login error:', errorCode, errorMessage);
       });
   };
 
@@ -61,15 +82,20 @@ const Login = () => {
                 required
               />
             </div>
+            {error && (
+              <div className='mb-4 text-red-500 text-center'>
+                {error}
+              </div>
+            )}
             <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
               Login
             </button>
           </form>
           <div className='flex text-sm gap-2 justify-center mt-1'>
-          <p className='text-white'> New here?</p>
-          <Link href='/signup'>
-            <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Create your account now!</button>
-          </Link>
+            <p className='text-white'> New here?</p>
+            <Link href='/signup'>
+              <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Create your account now!</button>
+            </Link>
           </div>
         </div>
       </div>
