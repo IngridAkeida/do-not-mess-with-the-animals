5da65eaabc7c6e6e2760f0d362da279ff9822a6b
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-08 14:43:28 +0200

    refactor: Implement lazy loading for genre menu items in genre page components

--- src/components/Header/Nav/DropDownMenu/DropDownMenu.jsx
+++ src/components/Header/Nav/DropDownMenu/DropDownMenu.jsx
@@ -1,33 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 import { NavigationInfo } from '../../../uiComponents/MenuList/MenuList';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const DropDownMenu = () => {
   const [openDropdown, setOpenDropdown] = useState(null);
   const navigationInfo = NavigationInfo;
-  const router = useRouter();
   const dropdownRef = useRef(null);
-
-  const handleOptionClick = (href) => {
-    router.push(href);
-  };
 
   const toggleDropdown = (name) => {
     setOpenDropdown(openDropdown === name ? null : name);
   };
-
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setOpenDropdown(null);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
 
   return (
     <div className='hidden sm:flex gap-x-2 lg:gap-x-6'>
@@ -40,23 +22,21 @@ const DropDownMenu = () => {
           {item.name}
         </button>
         {openDropdown === item.name && (
-          <div className='absolute top-11 left-0 bg-dark-neutral-a40 rounded shadow-lg border border-dark-menu-y10'>
+          <div className='absolute top-11 left-0 bg-dark-neutral-a40 rounded shadow-lg border border-dark-menu-y10 flex flex-col'>
             {item.subMenu
               ? item.subMenu.map((subItem) => (
-                  <div
+                  <Link
                     key={subItem.name}
                     className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10 hover:text-dark-neutral-a40'
-                    onClick={() => handleOptionClick(subItem.href)}
+                    href={subItem.href}
                   >
                     {subItem.name}
-                  </div>
+                  </Link>
                 ))
               : (
                 <div
-                  className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10'
-                  onClick={() => handleOptionClick(item.href)}
-                >
-                  {item.name}
+                  className='cursor-pointer px-4 py-2 text-sm font-semibold text-alert-danger-100 hover:bg-dark-menu-y10'>
+                  not found
                 </div>
               )}
           </div>
