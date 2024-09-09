41a2dfaa4c2f921c062e920ba79de5412a29efaa
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-09 17:29:51 +0200

    refactor: Update genre page components to remove unnecessary hover animation

--- src/app/movies/popular/page.jsx
+++ src/app/movies/popular/page.jsx
@@ -67,7 +67,7 @@ const MoviesPopularPage = () => {
             </h1>
             <div className='flex flex-wrap flex-row gap-4 justify-center items-center my-4 pb-4'>
               {list[toggle ? 1 : 0].items.results.map((item, index) => (
-                  <div key={index} className='h-80 w-52 border rounded-md flex flex-col items-center justify-center bg-dark-accent-a40 hover:bg-dark-accent-a30 transition duration-300 cursor-pointer text-white hover:text-dark-accent-a0 hover:animate-jump animate-once animate-duration-1000 animate-ease-in-out'>
+                  <div key={index} className='h-80 w-52 border rounded-md flex flex-col items-center justify-center bg-dark-accent-a40 hover:bg-dark-accent-a30 transition duration-300 cursor-pointer text-white hover:text-dark-accent-a0 '>
                     {item.title}
                   </div>
               ))}
