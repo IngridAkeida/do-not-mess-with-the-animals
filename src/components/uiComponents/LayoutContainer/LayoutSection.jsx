aa5c7a4c87ba80787e7e1b3745786e6e417299ef
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-11 15:16:34 +0200

    refactor: Update LayoutSection component to use theme for background color
    This commit updates the LayoutSection component to use the theme provided by the `next-themes` package for setting the background color. It replaces the hard-coded gradient background with a dynamic background color that changes based on the selected theme. This enhancement improves the consistency and flexibility of the component, providing a better user experience across different themes.

--- src/components/uiComponents/LayoutContainer/LayoutSection.jsx
+++ src/components/uiComponents/LayoutContainer/LayoutSection.jsx
@@ -1,6 +1,9 @@
+import { useTheme } from 'next-themes';
+
 const LayoutSection = ({ children }) => {
+  const { theme } = useTheme();
   return (
-    <div className='bg-gradient-to-br from-dark-primary-a40 via-dark-primary-a30 to-dark-primary-a40'>
+    <div className={`p-6 ${theme === 'dark' ? 'bg-dark-primary-a40 text-white' : 'bg-light-primary-100 text-dark-primary-a40'}`}>
       {children}
     </div>
   );
