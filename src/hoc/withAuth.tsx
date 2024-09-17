5fbe5834e47cfb6a7c0f8f311da4b13c117e00a9
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-17 16:13:41 +0200

    refactor: Update withAuth hoc to use displayName for wrapped component
    This commit updates the withAuth higher-order component (HOC) to set the displayName property for the wrapped component. By setting the displayName, it provides a more descriptive name for the component when debugging or inspecting the React component tree. This improves code readability and maintainability.

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -3,26 +3,31 @@ import { useRouter } from 'next/navigation';
 import { useAuth } from '../context/AuthContext';
 
 const withAuth = (WrappedComponent: React.ComponentType) => {
-  return (props: any) => {
+  const ComponentWithAuth = (props: any) => {
     const { user, loading } = useAuth();
     const router = useRouter();
 
     useEffect(() => {
       if (!loading && !user) {
-        router.push('/login'); // Redireciona para a página de login se não estiver autenticado
+        router.push('/login');
       }
     }, [user, loading, router]);
 
     if (loading) {
-      return <div>Loading...</div>; // Exibe um indicador de carregamento enquanto verifica a autenticação
+      return <div>Loading...</div>;
     }
 
     if (!user) {
-      return null; // Redireciona para login, mas o useEffect acima já está lidando com isso
+      return null;
     }
 
     return <WrappedComponent {...props} />;
   };
+
+  ComponentWithAuth.displayName = `withAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return ComponentWithAuth;
 };
 
 export default withAuth;
+
