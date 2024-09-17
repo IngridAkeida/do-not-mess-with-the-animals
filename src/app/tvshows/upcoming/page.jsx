8f423bbf1854f3564732afb8dc1a6f7665da7b79
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-17 16:17:56 +0200

    refactor: Update TVShowsUpcomingPage component to use lazy loading for images
    This commit updates the TVShowsUpcomingPage component to implement lazy loading for images using the next/image package. Lazy loading improves page load performance by loading images only when they are visible in the viewport. This refactor enhances the user experience and optimizes the rendering of the component.

--- src/app/tvshows/upcoming/page.jsx
+++ src/app/tvshows/upcoming/page.jsx
@@ -1,37 +1,81 @@
-//const TVShowsUpcomingPage = () => {
-  // const [list, setList] = useState([]);
-  // const [error, setError] = useState('');
-  // const [loading, setLoading] = useState(true);
+'use client';
+import { useEffect, useState } from 'react';
+import { getListTvShowNew } from '@/pages/api/dataTMDBTvShowNew';
+import Layout from '@/components/uiComponents/LayoutContainer/LayoutContainer';
+import Link from 'next/link';
+import Image from 'next/image';
 
-  // useEffect(() => {
-  //   const loadAllGenres = async () => {
-  //     try {
-  //       const listPopular = await getListTvshowNew();
-  //       if (listPopular) {
-  //         setList(listPopular);
-  //       } else {
-  //         setError('Failed to load.');
-  //       }
-  //     } catch (e) {
-  //       setError('Failed to load.');
-  //       console.error(e);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   loadAllGenres();
-  // }, []);
+const TVShowsUpcomingPage = () => {
+  const [list, setList] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [toggle, setToggle] = useState(false);
 
-  // if (loading) {
-  //   return <div className='text-white'>Loading...</div>;
-  // }
+  useEffect(() => {
+    const loadAllGenres = async () => {
+      try {
+        const listPopular = await getListTvShowNew();
+        if (listPopular) {
+          setList(listPopular);
+        } else {
+          setError('Failed to load.');
+        }
+      } catch (e) {
+        setError('Failed to load.');
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadAllGenres();
+  }, []);
 
-  // if (error) {
-  //   return <div className='text-white'>Error: {error}</div>;
-  // }
-//   return (
-//     <div>here</div>
-//   );
-// }
+  if (loading) {
+    return <div className='text-white'>Loading...</div>;
+  }
 
-// export default TVShowsUpcomingPage;
+  if (error) {
+    return <div className='text-white'>Error: {error}</div>;
+  }
+  return (
+    <Layout>
+      <div className='bg-gradient-to-br from-dark-primary-a40 via-dark-primary-a30 to-dark-primary-a40'>
+        <div className='relative'>
+          <div className='absolute top-4 right-24'>
+            <label className='inline-flex items-center cursor-pointer'>
+              <input
+                type='checkbox'
+                className='sr-only'
+                checked={toggle}
+                onChange={() => setToggle(!toggle)}
+              />
+              <div className='block border border-dark-primary-a20 bg-dark-menu-y10 w-28 h-8 rounded-full'></div>
+              <div
+                className={`dot absolute left-1 top-1 bg-dark-primary-a20 w-14 h-6 px-2 text-center rounded-full duration-300 transition ${
+                  toggle ? 'transform translate-x-12' : ''
+                }`}
+              >{toggle ? 'Week':'Day'}</div>
+            </label>
+          </div>
+          <div key={list[toggle ? 1 : 0]?.name}>
+            <h1 className='text-center text-2xl p-4 text-white font-semibold'>
+              {toggle ? 'Trending Movies' : 'Trending Movies'}
+            </h1>
+            <div className='flex flex-wrap flex-row gap-4 justify-center text-center items-center pb-4'>
+              {list[toggle ? 1 : 0].items.results.map((item, index) => (
+                <Link key={index} href={`/movie/${item.id}`}>
+                  <div  className='h-80 w-52 border rounded-md flex flex-col items-center justify-center bg-dark-accent-a40 hover:bg-dark-accent-a30 transition duration-300 cursor-pointer text-white hover:text-dark-accent-a0 '>
+                    <Image src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} className='rounded-md h-80' alt={item.title} width={400} height={500}/>
+                    <div className='absolute w-48'>{item.title || item.name}</div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default TVShowsUpcomingPage;
