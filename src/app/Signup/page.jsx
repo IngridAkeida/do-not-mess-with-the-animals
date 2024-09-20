479631e3818ddac0cd78f3a50133d7c579268958
IngridAkeida <ingridsanchesp@gmail.com>
2024-09-20 15:11:27 +0200

    refactor: Use LayoutForm component in Signup page
    This commit refactors the Signup page by replacing the existing form layout with the LayoutForm component. The LayoutForm component provides a consistent layout for forms across the application, improving code maintainability and enhancing the visual presentation of the Signup page.

--- src/app/Signup/page.jsx
+++ src/app/Signup/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { auth } from '@/lib/firebaseData';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import LayoutForm from '@/components/uiComponents/LayoutContainer/LayoutForm';
 import Nav from '@/components/Header/Nav/Nav';
 import Link from 'next/link';
 
@@ -69,70 +70,65 @@ const SignUp = () => {
   };
 
   return (
-    <div className='max-w-7xl mx-auto bg-dark-primary-a40'>
-      <Nav />
-      <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-dark-primary-a30 via-dark-primary-a20 to-dark-primary-a30 text-dark-neutral-a40'>
-        <div className='bg-dark-accent-a40 p-8 rounded shadow-md w-80 border border-dark-menu-y10'>
-          <h2 className='text-2xl mb-4 text-center font-extrabold text-dark-menu-y10'>Sign Up</h2>
-          <form onSubmit={handleSignup}>
-            <div className='mb-4'>
-              <label className='block font-bold text-dark-menu-y10'>Name</label>
-              <input
-                type='text'
-                value={name}
-                onChange={handleNameChange}
-                className='w-full px-3 py-2 border rounded'
-                required
-              />
-            </div>
-            <div className='mb-4'>
-              <label className='block font-bold text-dark-menu-y10'>Email</label>
-              <input
-                type='email'
-                value={email}
-                onChange={handleEmailChange}
-                className='w-full px-3 py-2 border rounded'
-                required
-              />
-            </div>
-            <div className='mb-4'>
-              <label className='block font-bold text-dark-menu-y10'>Password</label>
-              <input
-                type='password'
-                value={password}
-                onChange={handlePasswordChange}
-                className='w-full px-3 py-2 border rounded'
-                required
-              />
-            </div>
-            <div className='mb-4'>
-              <label className='block font-bold text-dark-menu-y10'>Re-enter password</label>
-              <input
-                type='password'
-                value={confPassword}
-                onChange={handleConfirmPasswordChange}
-                className='w-full px-3 py-2 border rounded'
-                required
-              />
-            </div>
-            {error && (
-              <div className='mb-4 text-red-500 text-center'>
-                {error}
-              </div>
-            )}
-            <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
-              Sign Up
-            </button>
-          </form>
-          <div className='flex text-sm gap-2 justify-center mt-1'>
-            <p className='text-white'> Already have an account?</p>
-            <Link href='/login'>
-              <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Sign in</button>
-            </Link>
-          </div> 
-        </div> 
-      </div>
-    </div>
+    <LayoutForm>
+      <h2 className='text-2xl mb-4 text-center font-extrabold text-dark-menu-y10'>Sign Up</h2>
+      <form onSubmit={handleSignup}>
+        <div className='mb-4'>
+          <label className='block font-bold text-dark-menu-y10'>Name</label>
+          <input
+            type='text'
+            value={name}
+            onChange={handleNameChange}
+            className='w-full px-3 py-2 border rounded'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='block font-bold text-dark-menu-y10'>Email</label>
+          <input
+            type='email'
+            value={email}
+            onChange={handleEmailChange}
+            className='w-full px-3 py-2 border rounded'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='block font-bold text-dark-menu-y10'>Password</label>
+          <input
+            type='password'
+            value={password}
+            onChange={handlePasswordChange}
+            className='w-full px-3 py-2 border rounded'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label className='block font-bold text-dark-menu-y10'>Re-enter password</label>
+          <input
+            type='password'
+            value={confPassword}
+            onChange={handleConfirmPasswordChange}
+            className='w-full px-3 py-2 border rounded'
+            required
+          />
+        </div>
+        {error && (
+          <div className='mb-4 text-red-500 text-center'>
+            {error}
+          </div>
+        )}
+        <button type='submit' className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600'>
+          Sign Up
+        </button>
+      </form>
+      <div className='flex text-sm gap-2 justify-center mt-1'>
+        <p className='text-white'> Already have an account?</p>
+        <Link href='/login'>
+          <button type='button' className='text-white font-semibold hover:text-dark-menu-y10'>Sign in</button>
+        </Link>
+      </div> 
+    </LayoutForm>
   );
 };
 
