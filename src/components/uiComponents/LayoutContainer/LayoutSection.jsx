a6ea01a28ddbcc3fa71e3ba6191339cde6b1d8ae
IngridAkeida <ingridsanchesp@gmail.com>
2024-10-14 17:49:37 +0200

    refactor: Update LayoutSection component to improve visual presentation
    The LayoutSection component has been updated to improve its visual presentation. The background colors for both light and dark themes have been adjusted to enhance the overall user experience. The changes ensure a consistent appearance and make the component more visually appealing.

--- src/components/uiComponents/LayoutContainer/LayoutSection.jsx
+++ src/components/uiComponents/LayoutContainer/LayoutSection.jsx
@@ -5,16 +5,16 @@ const LayoutSection = ({ children, adicionalStyle }) => {
   const { theme } = useTheme();
   useEffect(() => {
     if (theme === 'dark') {
-      document.body.style.backgroundColor = '#000000'; 
+      document.body.style.backgroundColor = '#1a0303'; 
       document.body.style.color = '#FFFFFF'; 
     } else {
-      document.body.style.backgroundColor = '#f8f8f8'; 
+      document.body.style.backgroundColor = '#c4afaf'; 
       document.body.style.color = '#ffffff'; 
     }
   }, [theme]);
 
   return (
-    <div className={`${adicionalStyle} p-6 ${theme === 'dark' ? 'bg-dark-neutral-a40  text-white' : 'bg-white  text-dark-neutral-a40 '}`}>
+    <div className={`${adicionalStyle} p-6 ${theme === 'dark' ? 'bg-dark-primary-a40 text-white' : 'bg-white  text-dark-neutral-a40 '}`}>
       {children}
     </div>
   );
