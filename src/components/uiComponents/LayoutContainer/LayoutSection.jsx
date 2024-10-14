781ac7997a074dba85ba500567abe0d7b72b4f53
IngridAkeida <ingridsanchesp@gmail.com>
2024-10-14 17:27:43 +0200

    refactor: Update global CSS variables and body styles
    This commit updates the global CSS variables and body styles to improve the visual presentation of the application. The changes include adjusting the foreground and background colors for both light and dark themes. By updating these styles, we ensure a consistent appearance and enhance the overall user experience.

--- src/components/uiComponents/LayoutContainer/LayoutSection.jsx
+++ src/components/uiComponents/LayoutContainer/LayoutSection.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { useTheme } from 'next-themes';
 
 const LayoutSection = ({ children, adicionalStyle }) => {
   const { theme } = useTheme();
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.style.backgroundColor = '#000000'; 
+      document.body.style.color = '#FFFFFF'; 
+    } else {
+      document.body.style.backgroundColor = '#ffffff'; 
+      document.body.style.color = '#000000'; 
+    }
+  }, [theme]);
+
   return (
-    <div className={`${adicionalStyle} p-6 ${theme === 'dark' ? 'bg-gradient-to-br from-dark-primary-a40 via-dark-primary-a30 to-dark-primary-a40 text-white' : 'bg-light-primary-100 text-dark-primary-a40'}`}>
+    <div className={`${adicionalStyle} p-6 ${theme === 'dark' ? 'bg-dark-neutral-a40  text-white' : 'bg-dark-neutral-a0 text-dark-neutral-a40 '}`}>
       {children}
     </div>
   );
