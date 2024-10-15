1a486779f04e4b3bd968bdb0601baacd1bd3665a
IngridAkeida <ingridsanchesp@gmail.com>
2024-10-15 15:35:37 +0200

    refactor: Update LayoutCards component to use next-themes for dynamic theming
    The LayoutCards component has been updated to import and use the `useTheme` hook from the `next-themes` library. This allows the component to dynamically apply different styles based on the current theme. The `theme` variable is now used to conditionally set the background color, text color, and hover styles. This change improves the visual presentation and enhances the user experience by providing a consistent theming experience across the application.

--- src/components/uiComponents/Layouts/LayoutCards.jsx
+++ src/components/uiComponents/Layouts/LayoutCards.jsx
@@ -1,7 +1,9 @@
-
+import { useTheme } from 'next-themes';
 const LayoutCards = ({ children }) => {
+  const { theme } = useTheme();
   return (
-    <div className='h-80 w-52 border rounded-md flex flex-col items-center justify-center bg-dark-accent-a40 hover:bg-dark-accent-a30 transition duration-300 cursor-pointer text-white hover:text-dark-accent-a0 hover:animate-jump animate-once animate-duration-1000 animate-ease-in-out'>
+    <div className={`h-80 w-52 rounded-md flex flex-col items-center justify-center transition duration-300 cursor-pointer hover:animate-jump animate-once animate-duration-1000 animate-ease-in-out
+    ${theme === 'dark' ?  'bg-dark-accent-a40 hover:bg-dark-accent-a30 hover:text-dark-accent-a0  text-white' : 'bg-dark-primary-a30 hover:bg-dark-primary-a40  text-white'}`}>
       {children}
     </div>
   );
