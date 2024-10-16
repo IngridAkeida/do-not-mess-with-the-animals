e936b2ce40f304936a0247a34966eca37bf3dd73
IngridAkeida <ingridsanchesp@gmail.com>
2024-10-16 17:59:21 +0200

    refactor: Update PersonPage component layout and styling
    The PersonPage component has been updated to improve the layout and styling. Changes include:
    - Reorganized the structure of the component for better readability and maintainability.
    - Added a new section to display the person's known works.
    - Updated the styling of the person's biography section for better readability.
    - Adjusted the layout and sizing of the person's profile image.
    
    This commit improves the visual presentation and enhances the user experience of the PersonPage component.

--- src/app/person/[id]/page.jsx
+++ src/app/person/[id]/page.jsx
@@ -10,7 +10,6 @@ const PersonPage = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
-
   useEffect(() => {
     if (!id) return; 
 
@@ -32,6 +31,8 @@ const PersonPage = () => {
     fetchPerson();
   }, [id]);
 
+  console.log(person);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -46,21 +47,50 @@ const PersonPage = () => {
   return (
     <Layout>
       <LayoutSection>
-        <div className='flex gap-2'>
-          <div>
-            <h1 className='text-3xl'>{person.name}</h1>
-            <p>{person.known_for_department}</p>
-            <p>{person.place_of_birth}</p>
-            <Image 
-              src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} 
-              alt={person.name} 
-              width={500}
-                                height={750}
-                                priority={true}
-            />
+        <div className='flex justify-center items-center'>
+          <div className='max-w-6xl gap-2'>
+        <div className='flex gap-x-4 bg-dark-primary-a30 rounded-md text-white'>
+          <Image 
+                src={`https://image.tmdb.org/t/p/w500${person.profile_path}`} 
+                alt={person.name} 
+                width={500}
+                height={750}
+                priority={true}
+                className='rounded-md w-52  '
+              />
+              <div className='mb-4'>
+                <h1 className='text-3xl'>{person.name}</h1>
+                <p>{person.known_for_department}</p>
+                <p>{person.place_of_birth}</p>
+                <div>
+                  <h2>known for:</h2>
+                  <ul className='flex gap-x-2'>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                    <li className='w-36 h-48 bg-white rounded-md'>.</li>
+                  </ul>
+                </div>
+              </div>
           </div>
-        
-        <p className='text-justify '>{person.biography}</p>
+          <div className='bg-dark-menu-y10 p-4'>
+            <p className='text-justify text-base leading-relaxed text-gray-700 dark:text-gray-300 flex flex-col'>
+              <span className='font-bold text-lg text-center pb-4 '>
+                "{person.biography.split('.')[0]}"
+              </span>
+              <span>
+                <span className='opacity-0'>{'-----'}</span>
+                {person.biography.slice(person.biography.indexOf('.') + 1).split(/(\(.*?\))/g).map((text, index) => (
+                  <span key={index} className={text.startsWith('(') && text.endsWith(')') ? 'font-bold' : ''}>
+                    {text}
+                  </span>
+                ))}
+              </span>
+            </p>
+          </div>
+          </div>
         </div>
       </LayoutSection>
     </Layout>
