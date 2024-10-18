048072192913624384d68b83c828f3bf73d40ef5
IngridAkeida <ingridsanchesp@gmail.com>
2024-10-18 11:26:02 +0200

    refactor: Improve layout and styling of CardLayout component
    Enhance the visual presentation and user experience of the CardLayout component by updating its layout and styling. Changes include adding a hover effect to the card, updating the position and styling of the action buttons, and improving the alignment and spacing of the content. This commit improves the visual presentation and user experience of the CardLayout component.

--- src/components/Main/CardLayout/CardLayout.jsx
+++ src/components/Main/CardLayout/CardLayout.jsx
@@ -12,7 +12,7 @@ const CardLayout = ({result}) => {
 
   return(
     <div className='sm:h-60 sm:w-40 relative sm:cursor-pointer'>
-      <div className='absolute text-left text-white bg-gradient-to-r sm:bg-gradient-to-t from-dark-neutral-a50  sm:hover:from-dark-neutral-a50 sm:from-transparent to-transparent h-[100%] w-[100%] rounded-xl flex flex-col justify-center sm:justify-end items-start sm:text-center sm:pb-4'>
+      <div className='absolute bg-black opacity-0 hover:opacity-80 sm:h-60 sm:w-40 rounded-xl flex justify-center items-center'>
         <div className='block sm:text-transparent sm:hover:text-white px-2 h-[80%] w-[100%]'>
           <div className='flex sm:flex-col gap-2 sm:gap-0'>
             <p className='font-bold'>{result.name || result.title}</p>
@@ -20,6 +20,13 @@ const CardLayout = ({result}) => {
           </div>
           <p className='text-xs pb-1'>{result.releaseYear}</p>
           {result.genre_ids && <p>Genre: {result.genre_ids}</p>}
+          <div className='text-sm flex gap-2 absolute bottom-2 sm:bottom-4 sm:left-12'>
+            <span onClick={handleAddClick} className='cursor-pointer'>
+              {isAdded ? <FaCheck /> : <FaPlus />}
+            </span>
+            <IsFavoriteComponent itemId={result.id} />
+            <FaPlay />
+          </div> 
           {/* ajustar esse componente asap */}
           {/* <div className=''>
             {result.additionalData && result.additionalData.length > 0 && 
@@ -44,24 +51,24 @@ const CardLayout = ({result}) => {
               ))
             }
           </div> */}
-          <div className='text-sm flex gap-2 absolute bottom-2 sm:bottom-4 sm:left-12'>
+          {/* <div className='text-sm flex gap-2 absolute bottom-2 sm:bottom-4 sm:left-12'>
             <span onClick={handleAddClick} className='cursor-pointer'>
               {isAdded ? <FaCheck /> : <FaPlus />}
             </span>
             <IsFavoriteComponent itemId={result.id} />
             <FaPlay />
-          </div>
+          </div> */}
         </div>
       </div>
       <div className='flex sm:hidden'>{result.backdrop_path === null ? (
-            <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={result.name || result.title} />
-          ) : (
-            <Image className='rounded-xl' src={`https://image.tmdb.org/t/p/w500/${result.backdrop_path}`} width={500}
-            height={750}
-            priority={true} alt={result.name || result.title} />
-          )}
-        </div>
-      <div className='hidden sm:flex h-[100%]'>{result.poster_path=== null ? (
+          <Image className='rounded-xl' src='/assets/movie-nf-hor.png' width={300} height={300} alt={result.name || result.title} />
+        ) : (
+          <Image className='rounded-xl' src={`https://image.tmdb.org/t/p/w500/${result.backdrop_path}`} width={500}
+          height={750}
+          priority={true} alt={result.name || result.title} />
+        )}
+      </div>
+      <div className='hidden sm:flex h-[100%] hover:opacity-70'>{result.poster_path=== null ? (
           <Image className='rounded-xl' src='/assets/movie-nf.png' width={500} height={500} alt={result.name || result.title} />
         ) : (
           <Image className='rounded-xl' src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`} width={500}
